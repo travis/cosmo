aadd0e7e7a1661b3893d115f9b390f019ad242b9
bcm <bcm@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2005-06-04 02:41:04 +0000

    add constants class
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@298 5fd0b11a-f2f7-0310-948e-c8f80ebed640

---
+++ src/main/java/org/osaf/cosmo/CosmoConstants.java
@@ -0,0 +1,17 @@
+package org.osaf.cosmo;
+
+/**
+ * Constant definitions for Cosmo
+ */
+public class CosmoConstants {
+    /**
+     * The servlet context attribute which contains the Cosmo version
+     * number.
+     */
+    public static final String SC_ATTR_VERSION = "cosmo.version";
+    /**
+     * The servlet context attribute which contains the Cosmo server
+     * administrator's email address.
+     */
+    public static final String SC_ATTR_SERVER_ADMIN = "cosmo.serverAdmin";
+}

b5d160f7ee2aa0199437efd4a0cde3caec109153
bcm <bcm@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2005-06-15 05:10:26 +0000

    add jcr constants interface
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@335 5fd0b11a-f2f7-0310-948e-c8f80ebed640

---
+++ src/main/java/org/osaf/cosmo/jcr/CosmoJcrConstants.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2005 Open Source Applications Foundation
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.osaf.cosmo.jcr;
+
+import org.apache.jackrabbit.JcrConstants;
+
+/**
+ * Extends {@link org.apache.jackrabbit.JcrConstants} to
+ * provide additional constants for JCR items, node types
+ * etc. implemented by Cosmo.
+ */
+public interface CosmoJcrConstants extends JcrConstants {
+
+    // node types
+
+    public static final String NT_TICKET = "cosmo:ticket";
+
+    // node properties
+
+    public static final String NP_OWNER = "cosmo:owner";
+    public static final String NP_TIMEOUT = "cosmo:timeout";
+    public static final String NP_READ = "cosmo:read";
+    public static final String NP_WRITE = "cosmo:write";
+    public static final String NP_CREATED = "cosmo:created";
+    public static final String NP_LAST_MODIFIED = "cosmo:lastModified";
+}

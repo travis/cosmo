7533fc159d73d139cc551f2430411b792834255d
todd <todd@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2005-11-20 08:01:45 +0000

    Initial version.
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@957 5fd0b11a-f2f7-0310-948e-c8f80ebed640

---
+++ src/test/bin/hammer/createaccounts.py
@@ -0,0 +1,160 @@
+#! /usr/bin/env python
+
+# 
+# Copyright 2005 Kapor Enterprises, Inc. and Todd Agulnick.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at 
+# 
+# 	http://www.apache.org/licenses/LICENSE-2.0
+# 	
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
+# createaccounts.py: Create test accounts on a Cosmo server.
+#
+
+
+"""
+Create Cosmo Test Accounts
+
+Creates test user accounts on a Cosmo server. Each test account is created
+with username "test{seqnum}" where {seqnum} is a number created from the
+sequence determined by the first sequence number parameter and the number
+of accounts parameter. The password for each account is the same as the
+account's username.
+
+Usage: python createaccounts.py [options]
+
+Options:
+  -s        servername (default is cosmo-test.osafoundation.org)
+  -p        port (default is 8201)
+  -u        username (default is root)
+  -w        password (default is cosmo)
+  -n        number of accounts to create (default is 100)
+  -f        first account number in sequence (default is 1)
+  -h        display this help text
+
+Examples:
+  createaccounts.py Create 100 accounts starting with test1.
+  createaccounts.py -f 101 -n 100 Create accounts test101 through test200.
+"""
+
+from threading import Thread
+import pycurl
+import sys, getopt, time
+
+def createAccount(server, port, username, password, adminuser, adminpass):
+    xmlCreateAccount = """<?xml version="1.0" encoding="utf-8" ?> 
+<user xmlns="http://osafoundation.org/cosmo">
+  <username>%s</username>
+  <password>%s</password>
+  <firstName>Tommy</firstName>
+  <lastName>Tester</lastName>
+  <email>%s@nojunkmailplease.com</email>
+</user>
+""" % (username, password, username)
+
+    body = StringAsFile(xmlCreateAccount.encode("utf-8"))
+    url = "http://%s:%s@%s:%d/api/user/%s" % (adminuser, adminpass, server, port, username)
+    response = StringAsFile()
+    curl = pycurl.Curl()
+    curl.setopt(pycurl.URL, url)
+    curl.setopt(pycurl.UPLOAD, 1)
+    curl.setopt(pycurl.NOSIGNAL, 1)
+    curl.setopt(pycurl.READFUNCTION, body.read)
+    curl.setopt(pycurl.HTTPHEADER, ["Content-Length: %d" % len(str(body)), "Content-Type: text/xml"])
+    curl.setopt(pycurl.VERBOSE, 0)
+    curl.setopt(pycurl.HEADERFUNCTION, response.write)
+    curl.setopt(pycurl.TIMEOUT, 10)
+    try:
+        curl.perform()
+    except:
+        import traceback
+        traceback.print_exc(file=sys.stderr, limit=1)
+        sys.stderr.flush()
+        return False
+
+    code = curl.getinfo(pycurl.HTTP_CODE)
+    if code == 201:
+        print "Created user account %s" % username
+        return True
+    elif code == 204:
+        print "WARNING: Account %s already exists." % username
+        return True
+    elif code == 401:
+        print "\nERROR: Authorization error. Check username and password."
+        return False
+    else:
+        print "Unhandled response code: %d" % code
+        return False
+
+    return True
+
+class StringAsFile:
+    def __init__(self, str = ""):
+        self.str = str
+        self.index = 0
+
+    def read(self, size):
+        p = self.index
+        numchars = int(size / 2)
+        self.index += numchars
+        return self.str[p:p+numchars]
+    
+    def write(self, str):
+        self.str += str
+
+    def __repr__(self):
+        return self.str
+
+def usage():
+    print __doc__
+    
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, "s:p:u:w:n:f:h")
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+
+    # establish defaults
+    server = "cosmo-demo.osafoundation.org"
+    port = 8201
+    username = "root"
+    password = "cosmo"
+    start = 1
+    count = 100
+    
+    for (opt, arg) in opts:
+        if opt =="-s":      server = arg
+        elif opt == "-p":   port = int(arg)
+        elif opt == "-u":   username = arg
+        elif opt == "-w":   password = arg
+        elif opt == "-n":   count = int(arg)
+        elif opt == "-f":   start = int(arg)
+        elif opt == "-h":
+            usage()
+            sys.exit()
+                
+    # We should ignore SIGPIPE when using pycurl.NOSIGNAL - see
+    # the libcurl tutorial for more info.
+    try:
+        import signal
+        from signal import SIGPIPE, SIG_IGN
+        signal.signal(signal.SIGPIPE, signal.SIG_IGN)
+    except ImportError:
+        pass
+
+    for i in range(start, start + count):
+        if createAccount(server, port, \
+            "test" + str(i), "test" + str(i), \
+            username, password) == False:
+            sys.exit(2)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])    
---
+++ src/test/bin/hammer/hammer.py
@@ -0,0 +1,225 @@
+#! /usr/bin/env python
+
+# 
+# Copyright 2005 Kapor Enterprises, Inc. and Todd Agulnick.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at 
+# 
+# 	http://www.apache.org/licenses/LICENSE-2.0
+# 	
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
+# hammer.py: Stress-tests a Cosmo server.
+#
+
+
+"""Hammer Cosmo Tester
+
+Executes stress tests the WebDAV portion of Cosmo.
+
+Usage: python hammer.py [options]
+
+Options:
+  -t        use specified number of threads (default is 10)
+  -i        specify number of GET/PUTs to do on each thread (default is 100)
+  -s        servername (default is cosmo-test.osafoundation.org)
+  -p        port (default is 8201)
+  -l        size of datafile to GET/PUT in bytes (default is 10000)
+  -u        username (default is test; thread number is appended to username)
+  -w        password (default is test; thread number is appended to password)
+  -m        timeout setting in seconds (default is 30)
+  -h        display this help text
+
+Examples:
+  hammer.py runs test using default parameters
+  hammer.py -p 80 runs default test on port 80
+
+Tip:
+  Use createaccounts.py to setup a batch of test accounts before running
+  this script.
+  
+"""
+
+from threading import Thread
+import pycurl
+import sys, getopt, time
+import string, random
+
+class TestCosmo(Thread):
+    def __init__(self, url, datalength, iterations, timeout):
+        Thread.__init__(self)
+        self.url = url
+        self.datalength = datalength
+        self.iterations = iterations
+        self.strToWrite = "";
+        self.strRead = "";
+        self.passed = False
+        self.timeout = timeout
+
+    def run(self):
+         for i in range(self.iterations):
+            self.strToWrite = StringAsFile(randomString(self.datalength))
+            self.strRead = StringAsFile()
+            if self.write(self.strToWrite) is not True: return
+            if self.read(self.strRead) is not True: return
+            if str(self.strToWrite) != str(self.strRead):
+                print "\nDownloaded string didn't match uploaded string"
+                return
+#            print "#",
+         self.passed = True
+
+    def write(self, fp):
+        curl = pycurl.Curl()
+        curl.setopt(pycurl.URL, self.url)
+        curl.setopt(pycurl.UPLOAD, 1)
+        curl.setopt(pycurl.NOSIGNAL, 1)
+        curl.setopt(pycurl.READFUNCTION, fp.read)
+        if self.timeout > 0: curl.setopt(pycurl.TIMEOUT, self.timeout)
+        try:
+            start = time.clock()
+            curl.perform()
+            print "PUT: %.3f" % (time.clock() - start)
+        except:
+            exctype, value = sys.exc_info()[:2]
+            if exctype == pycurl.error:
+                print "ERROR in PUT: %s" % value[1]
+            else:
+                print "ERROR in PUT: %s: %s" % (str(exctype), str(value))
+            return False
+        curl.close()
+        return True
+
+    def read(self, fp):
+        curl = pycurl.Curl()
+        curl.setopt(pycurl.URL, self.url)
+        curl.setopt(pycurl.WRITEFUNCTION, fp.write)
+        curl.setopt(pycurl.NOSIGNAL, 1)
+        if self.timeout > 0: curl.setopt(pycurl.TIMEOUT, self.timeout)
+        try:
+            start = time.clock()
+            curl.perform()
+            print "GET: %.3f" % (time.clock() - start)
+        except:
+            exctype, value = sys.exc_info()[:2]
+            if exctype == pycurl.error:
+                print "ERROR in GET: %s" % value[1]
+            else:
+                print "ERROR in GET: %s: %s" % (str(exctype), str(value))
+            return False
+        curl.close()
+        return True
+        
+class StringAsFile:
+    def __init__(self, str = ""):
+        self.str = str
+        self.index = 0
+
+    def read(self, size):
+        p = self.index
+        numchars = int(size / 2)
+        self.index += numchars
+        return self.str[p:p+numchars]
+    
+    def write(self, str):
+        self.str += str
+
+    def __repr__(self):
+        return self.str
+
+def randomString(length = 10000):
+    item = string.letters + string.digits
+    return "".join([random.choice(item) for i in range(length)])
+
+def usage():
+    print __doc__
+    
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, "t:i:s:p:l:u:w:m:ch")
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+
+    # establish defaults
+    threads = 10
+    iterations = 100
+    server = "cosmo-demo.osafoundation.org"
+    port = 8201
+    datalength = 10000
+    username = "test"
+    password = "test"
+    timeout = 30
+    
+    for (opt, arg) in opts:
+        if opt == "-t":      threads = int(arg)
+        elif opt == "-i":    iterations = int(arg)
+        elif opt == "-s":    server = arg
+        elif opt == "-p":    port = int(arg)
+        elif opt == "-l":    datalength = int(arg)
+        elif opt == "-u":    username = arg
+        elif opt == "-w":    password = arg
+        elif opt == "-m":    timeout = int(arg)
+        elif opt == "-h":
+            usage()
+            sys.exit()
+
+                
+    # We should ignore SIGPIPE when using pycurl.NOSIGNAL - see
+    # the libcurl tutorial for more info.
+    try:
+        import signal
+        from signal import SIGPIPE, SIG_IGN
+        signal.signal(signal.SIGPIPE, signal.SIG_IGN)
+    except ImportError:
+        pass
+
+    print "[%s] Starting test..." % time.ctime()
+    print "Running %d threads through %d iterations. File length is %d." % (threads, iterations, datalength)
+
+    startTime = time.time()    
+
+    # start up our threads
+    cosmotesters = []
+    for thread in range(1, threads + 1):
+        u = username + str(thread)
+        p = password + str(thread)
+        url = "http://%s:%s@%s:%d/home/%s/testfile.txt" % (u, p, server, port, u)
+        c = TestCosmo(url, datalength, iterations, timeout)
+        c.start()
+        cosmotesters.append(c)
+
+    failed = 0
+    
+    # wait for 'em all to finish
+    for c in cosmotesters:
+        c.join()
+        if c.passed == False: failed += 1
+
+    if failed > 0:
+        sf = "FAILED"
+    else:
+        sf = "PASSED"
+            
+    endTime = time.time()
+
+    elapsedTime = endTime - startTime
+    dataTransfered = iterations * threads * datalength * 2 / 1024
+    print ""
+    if failed: print "*** WARNING: %d tests of %d failed, %d passed." % (failed, threads, threads-failed)
+    print "Total Elapsed Time: %d seconds" % elapsedTime
+    print "Total data transfered: %d KB" % dataTransfered
+    print "Throughput: %d KB/sec" % (dataTransfered / elapsedTime)
+    
+    print "Ran %d threads through %d iterations." % (threads, iterations)
+    print "[%s] Test completed: %s." % (time.ctime(), sf)
+
+    if failed: sys.exit(2)    
+        
+if __name__ == "__main__":
+    main(sys.argv[1:])    

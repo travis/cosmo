6e98b8a0de0764db71bb265091327df2fa08c2a1
bcm <bcm@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2006-02-15 03:27:27 +0000

    add our first unit test for the dav layer. this comprises BaseDavServletTestCase, a base class for dav servlet unit tests, and DavBasicQueryTest, which provides one test, the equivalent of src/test/resources/dav/caldav/reports/basicquery/1.txt (tho it only checks the response status code and doesn't examine the contents of the response).
    report unit tests will place one or more events (currently only
    report-event1.ics) into a calendar collection and then execute a
    report request with content loaded from an xml file (currently only
    report-basic1.xml).
    
    dav layer unit tests require a real jcr session and use a full on
    jackrabbit repository. someday we can replace this with a mock
    (in-memory) jcr implementation, and these unit tests will run hella
    faster.
    
    
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@1430 5fd0b11a-f2f7-0310-948e-c8f80ebed640

---
+++ src/test/java/org/osaf/cosmo/dav/BaseDavServletTestCase.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2005 Open Source Applications Foundation
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.osaf.cosmo.dav;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+import javax.servlet.ServletContextEvent;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.jdom.Document;
+import org.jdom.input.SAXBuilder;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
+
+import org.osaf.cosmo.dao.jcr.JcrTestHelper;
+import org.osaf.cosmo.dao.mock.MockTicketDao;
+import org.osaf.cosmo.dav.CosmoDavServlet;
+import org.osaf.cosmo.jackrabbit.JackrabbitTestSessionManager;
+import org.osaf.cosmo.jackrabbit.query.TextFilterListener;
+import org.osaf.cosmo.model.User;
+import org.osaf.cosmo.security.mock.MockSecurityManager;
+import org.osaf.cosmo.security.mock.MockUserPrincipal;
+
+import org.springframework.mock.web.MockServletConfig;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+/**
+ * Base class for WebDAV+extensions servlet test cases.
+ */
+public abstract class BaseDavServletTestCase extends TestCase {
+    private static final Log log =
+        LogFactory.getLog(BaseDavServletTestCase.class);
+
+    private static final String SERVLET_PATH = "/home";
+
+    private JackrabbitTestSessionManager sessionManager;
+    protected MockSecurityManager securityManager;
+    protected JcrTestHelper testHelper;
+    protected MockServletContext servletContext;
+    protected CosmoDavServlet servlet;
+
+    /**
+     */
+    protected void setUp() throws Exception {
+        // XXX: refactor CosmoDavServlet.init so that we can provide
+        // our own mock session provider, locator factory, and
+        // resource factory; then we never need to actually hit the
+        // jcr repository, and we test this class in isolation rather
+        // than depending on all those other classes
+        sessionManager = new JackrabbitTestSessionManager();
+        sessionManager.setUp();
+
+        securityManager = new MockSecurityManager();
+
+        testHelper = new JcrTestHelper(sessionManager.getSession());
+
+        servletContext = new MockServletContext();
+
+        // load special query language extension
+        TextFilterListener listener = new TextFilterListener();
+        listener.contextInitialized(new ServletContextEvent(servletContext));
+
+        MockServletConfig servletConfig = new MockServletConfig(servletContext);
+        servletConfig.
+            addInitParameter(CosmoDavServlet.INIT_PARAM_RESOURCE_PATH_PREFIX,
+                             "/home");
+
+        servlet = new CosmoDavServlet();
+        servlet.setSecurityManager(securityManager);
+        servlet.setSessionFactory(sessionManager.getSessionFactory());
+        servlet.setTicketDao(new MockTicketDao());
+        servlet.init(servletConfig);
+    }
+
+    protected void tearDown() throws Exception {
+        servlet.destroy();
+
+        sessionManager.tearDown();
+    }
+
+    /**
+     */
+    protected MockHttpServletRequest createMockRequest(String method,
+                                                       String davPath) {
+        MockHttpServletRequest request =
+            new MockHttpServletRequest(servletContext, method,
+                                       SERVLET_PATH + davPath);
+        request.setServletPath(SERVLET_PATH);
+        request.setPathInfo(davPath);
+        return request;
+    }
+
+    /**
+     */
+    protected void logInUser(User user) {
+        securityManager.setUpMockSecurityContext(new MockUserPrincipal(user));
+    }
+
+    /**
+     */
+    protected void sendXmlRequest(MockHttpServletRequest request,
+                                  Document doc)
+        throws Exception {
+        XMLOutputter outputter = new XMLOutputter(Format.getCompactFormat());
+        ByteArrayOutputStream buf = new ByteArrayOutputStream();
+        outputter.output(doc, buf);
+        request.setContentType("text/xml");
+        request.setCharacterEncoding("UTF-8");
+        request.setContent(buf.toByteArray());;
+    }
+
+    /**
+     */
+    protected void sendXmlRequest(MockHttpServletRequest request,
+                                  byte[] xml)
+        throws Exception {
+        request.setContentType("text/xml");
+        request.setCharacterEncoding("UTF-8");
+        request.setContent(xml);
+    }
+
+    /**
+     */
+    protected Document readXmlResponse(MockHttpServletResponse response)
+        throws Exception {
+        ByteArrayInputStream in =
+            new ByteArrayInputStream(response.getContentAsByteArray());
+        SAXBuilder builder = new SAXBuilder(false);
+        return builder.build(in);
+    }
+}
---
+++ src/test/java/org/osaf/cosmo/dav/DavBasicQueryTest.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2005 Open Source Applications Foundation
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.osaf.cosmo.dav;
+
+import javax.jcr.Node;
+
+import net.fortuna.ical4j.model.Calendar;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.jdom.Document;
+
+import org.osaf.cosmo.dav.CosmoDavResponse;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+/**
+ * Test Case for DAV <code>REPORT</code> operations.
+ */
+public class DavBasicQueryTest extends BaseDavServletTestCase {
+    private static final Log log = LogFactory.getLog(DavBasicQueryTest.class);
+
+    private Node calendarCollectionNode;
+
+     /**
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        // make a calendar collection to put events into
+        calendarCollectionNode = testHelper.addCalendarCollectionNode();
+        // XXX: not sure why we have to save, since theoretically
+        // testHelper and the servlet are using the same jcr session
+        calendarCollectionNode.getParent().save();
+    }
+
+   /**
+     */
+    public void testBasicEvent() throws Exception {
+        // put a calendar resource into the calendar collection
+        Calendar calendar = testHelper.loadCalendar("report-event1.ics");
+        Node resourceNode =
+            testHelper.addCalendarResourceNode(calendarCollectionNode,
+                                               calendar);
+
+        // load the request content
+        Document content = testHelper.loadXml("report-basic1.xml");
+
+        // perform the report
+        MockHttpServletRequest request =
+            createMockRequest("REPORT", calendarCollectionNode.getPath()); 
+        sendXmlRequest(request, content);
+
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        servlet.service(request, response);
+
+        assertEquals(CosmoDavResponse.SC_MULTI_STATUS, response.getStatus());
+    }
+}

a2959600f4d948a53bc82197529969d1cdbd54dc
bcm <bcm@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2006-09-13 01:09:15 +0000

    add initial dav copy test. doesn't test anything other than the normal successful case yet.
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@2478 5fd0b11a-f2f7-0310-948e-c8f80ebed640

---
+++ src/test/unit/java/org/osaf/cosmo/dav/DavCopyTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2005-2006 Open Source Applications Foundation
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.osaf.cosmo.dav;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.osaf.cosmo.model.HomeCollectionItem;
+import org.osaf.cosmo.model.CollectionItem;
+import org.osaf.cosmo.model.ContentItem;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+/**
+ * Test case for <code>COPY</code> requests on
+ * <code>DavServlet</code>.
+ */
+public class DavCopyTest extends BaseDavServletTestCase {
+    private static final Log log = LogFactory.getLog(DavCopyTest.class);
+
+    /** */
+    public void testCopyContent() throws Exception {
+        HomeCollectionItem home = contentService.getRootItem(user);
+        ContentItem content = testHelper.makeDummyContent(user);
+        content.setAttribute("test:test", "test value");
+        contentService.createContent(home, content);
+
+        String oldPath = toCanonicalPath(content.getName());
+        String newPath = toCanonicalPath("testcopy");
+
+        MockHttpServletRequest request = createMockRequest("COPY", oldPath);
+        request.addHeader("Destination", toAbsoluteUrl(request, newPath));
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        servlet.service(request, response);
+
+        assertEquals("COPY content did not return Created",
+                     MockHttpServletResponse.SC_CREATED,
+                     response.getStatus());
+        assertNotNull("COPYd content does not exist at old location",
+                      contentService.findItemByPath(oldPath));
+
+        ContentItem newContent = (ContentItem)
+            contentService.findItemByPath(newPath);
+        assertNotNull("COPYd content does not exist at new location",
+                      newContent);
+        assertNotNull("COPYd content does not retain test property",
+                      newContent.getAttribute("test:test"));
+        assertEquals("COPYd content property does not have original value",
+                     "test value",
+                     newContent.getAttributeValue("test:test").toString());
+    }
+}

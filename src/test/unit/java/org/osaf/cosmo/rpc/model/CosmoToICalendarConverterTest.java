25349c94f66ef3e17d31b90c4b88ca2cdfaaf887
br <br@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2006-09-22 00:51:40 +0000

    work on recurrence editing.
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@2520 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- src/test/unit/java/org/osaf/cosmo/rpc/model/CosmoToICalendarConverterTest.java
+++ src/test/unit/java/org/osaf/cosmo/rpc/model/CosmoToICalendarConverterTest.java
@@ -15,23 +15,25 @@
  */
 package org.osaf.cosmo.rpc.model;
 
+import static org.osaf.cosmo.util.ICalendarUtils.getFirstEvent;
+import static org.osaf.cosmo.util.ICalendarUtils.getTZId;
+import static org.osaf.cosmo.util.ICalendarUtils.getUIDValue;
 import junit.framework.TestCase;
 import net.fortuna.ical4j.model.Calendar;
 import net.fortuna.ical4j.model.Component;
 import net.fortuna.ical4j.model.Date;
 import net.fortuna.ical4j.model.DateTime;
 import net.fortuna.ical4j.model.Property;
+import net.fortuna.ical4j.model.Recur;
+import net.fortuna.ical4j.model.TimeZone;
 import net.fortuna.ical4j.model.component.VEvent;
 import net.fortuna.ical4j.model.property.DtEnd;
 import net.fortuna.ical4j.model.property.DtStart;
+import net.fortuna.ical4j.model.property.RRule;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.osaf.cosmo.TestHelper;
-
-import static org.osaf.cosmo.util.ICalendarUtils.getFirstEvent;
-import static org.osaf.cosmo.util.ICalendarUtils.getUIDValue;
-import static org.osaf.cosmo.util.ICalendarUtils.getTZId;
 
 public class CosmoToICalendarConverterTest extends TestCase {
     private static final Log log = LogFactory.getLog(CosmoToICalendarConverterTest.class);
@@ -175,5 +177,172 @@ public class CosmoToICalendarConverterTest extends TestCase {
         DateTime dateTimeEnd = (DateTime) vevent.getEndDate().getDate();
         assertEquals("20051128T094500",dateTimeEnd.toString());
         assertEquals(getTZId(vevent.getEndDate()), "America/New_York");
+    }
+    
+    public void testCreateRecurringDailyNoEndDate(){
+        Event event = createBaseEvent();
+        RecurrenceRule rr = new RecurrenceRule();
+        rr.setFrequency(RecurrenceRule.FREQUENCY_DAILY);
+        rr.setEndDate(null);
+        event.setRecurrenceRule(rr);
+        
+        VEvent vevent = converter.createVEvent(event);
+        RRule rrule = (RRule) vevent.getProperties().getProperty(Property.RRULE);
+        assertNotNull(rrule);
+        
+        Recur recur = rrule.getRecur();
+        assertEquals(Recur.DAILY, recur.getFrequency());
+        assertNull(recur.getUntil());
+    }
+    
+    public void testCreateRecurringWeeklyNoEndDate(){
+        Event event = createBaseEvent();
+        RecurrenceRule rr = new RecurrenceRule();
+        rr.setFrequency(RecurrenceRule.FREQUENCY_WEEKLY);
+        rr.setEndDate(null);
+        event.setRecurrenceRule(rr);
+        
+        VEvent vevent = converter.createVEvent(event);
+        RRule rrule = (RRule) vevent.getProperties().getProperty(Property.RRULE);
+        assertNotNull(rrule);
+        
+        Recur recur = rrule.getRecur();
+        assertEquals(Recur.WEEKLY, recur.getFrequency());
+        assertNull(recur.getUntil());
+    }
+
+    public void testCreateRecurringBiWeeklyNoEndDate(){
+        Event event = createBaseEvent();
+        RecurrenceRule rr = new RecurrenceRule();
+        rr.setFrequency(RecurrenceRule.FREQUENCY_BIWEEKLY);
+        rr.setEndDate(null);
+        event.setRecurrenceRule(rr);
+        
+        VEvent vevent = converter.createVEvent(event);
+        RRule rrule = (RRule) vevent.getProperties().getProperty(Property.RRULE);
+        assertNotNull(rrule);
+        
+        Recur recur = rrule.getRecur();
+        assertEquals(Recur.WEEKLY, recur.getFrequency());
+        assertEquals(2, recur.getInterval());
+        assertNull(recur.getUntil());
+    }
+
+    public void testCreateRecurringMonthlyNoEndDate(){
+        Event event = createBaseEvent();
+        RecurrenceRule rr = new RecurrenceRule();
+        rr.setFrequency(RecurrenceRule.FREQUENCY_MONTHLY);
+        rr.setEndDate(null);
+        event.setRecurrenceRule(rr);
+        
+        VEvent vevent = converter.createVEvent(event);
+        RRule rrule = (RRule) vevent.getProperties().getProperty(Property.RRULE);
+        assertNotNull(rrule);
+        
+        Recur recur = rrule.getRecur();
+        assertEquals(Recur.MONTHLY, recur.getFrequency());
+        assertNull(recur.getUntil());
+    }
+
+    public void testCreateRecurringYearlyNoEndDate(){
+        Event event = createBaseEvent();
+        RecurrenceRule rr = new RecurrenceRule();
+        rr.setFrequency(RecurrenceRule.FREQUENCY_YEARLY);
+        rr.setEndDate(null);
+        event.setRecurrenceRule(rr);
+        
+        VEvent vevent = converter.createVEvent(event);
+        RRule rrule = (RRule) vevent.getProperties().getProperty(Property.RRULE);
+        assertNotNull(rrule);
+        
+        Recur recur = rrule.getRecur();
+        assertEquals(Recur.YEARLY, recur.getFrequency());
+        assertNull(recur.getUntil());
+    }
+
+    public void testCreateRecurringYearlyEndDate(){
+        Event event = createBaseEvent();
+        RecurrenceRule rr = new RecurrenceRule();
+        rr.setFrequency(RecurrenceRule.FREQUENCY_YEARLY);
+        
+        CosmoDate endDate = event.getStart().clone();
+        endDate.setYear(2010);
+        rr.setEndDate(endDate);
+        
+        event.setRecurrenceRule(rr);
+        
+        
+        VEvent vevent = converter.createVEvent(event);
+        RRule rrule = (RRule) vevent.getProperties().getProperty(Property.RRULE);
+        assertNotNull(rrule);
+        
+        Recur recur = rrule.getRecur();
+        assertEquals(Recur.YEARLY, recur.getFrequency());
+        
+        Date untilDate = recur.getUntil();
+        assertNotNull(untilDate);
+        java.util.Calendar calendar = getGMTCalendar(untilDate);
+        assertEquals(2010, calendar.get(java.util.Calendar.YEAR));
+    }
+    
+    /**
+     * Creates a very "normal" event, which you can use as a template for new test events.
+     *  
+     * This event:
+     * <ul>
+     *     <li>...is floating</li>
+     *     <li>...has no recurrence rule</li>
+     *     <li>id = "TESTID"</li>
+     *     <li>title = "Test Title"</li>
+     *     <li>description = "Test Description"</li>
+     *     <li>Start Date = 1/1/05 12pm </li>
+     *     <li>End Date = 1/1/05 1:30pm </li>
+     * </ul>  
+     * @return plain vanilla event
+     */
+    protected Event createBaseEvent(){
+        Event event = new Event();
+
+        event.setId("TESTID");
+        event.setMasterEvent(true);
+
+        event.setAllDay(false);
+        event.setAnyTime(false);
+        event.setPointInTime(false);
+        event.setRecurrenceRule(null);
+        
+        //20051128T074500
+        CosmoDate startDate = new CosmoDate();
+        startDate.setYear(2005);
+        startDate.setMonth(CosmoDate.MONTH_JANUARY);
+        startDate.setDate(1);
+        startDate.setHours(12);
+        startDate.setMinutes(0);
+        startDate.setSeconds(0);
+        startDate.setUtc(false);
+        
+        //20051128T084500
+        CosmoDate endDate = new CosmoDate();
+        endDate.setYear(2005);
+        endDate.setMonth(CosmoDate.MONTH_JANUARY);
+        endDate.setDate(1);
+        endDate.setHours(13);
+        endDate.setMinutes(30);
+        endDate.setSeconds(0);
+        endDate.setUtc(false);
+       
+        event.setStart(startDate);
+        event.setEnd(endDate);
+        
+        event.setDescription("Test Description");
+        event.setTitle("Test Title");
+        return event;
+    }
+    
+    protected java.util.Calendar getGMTCalendar(Date date){
+        java.util.Calendar calendar = java.util.Calendar.getInstance(TimeZone
+                .getTimeZone("GMT"));
+        calendar.setTime(date);
+        return calendar;
     }
 }

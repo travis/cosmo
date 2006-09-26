adaee73c300347f0e9ae4baaa36a0ad108dccc9e
bcm <bcm@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2006-09-26 18:45:51 +0000

    add new acl package with AclConstants and property.AlternateUriSet classes.
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@2543 5fd0b11a-f2f7-0310-948e-c8f80ebed640

---
+++ src/main/java/org/osaf/cosmo/dav/acl/AclConstants.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2005-2006 Open Source Applications Foundation
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.osaf.cosmo.dav.acl;
+
+import org.apache.jackrabbit.webdav.DavConstants;
+import org.apache.jackrabbit.webdav.property.DavPropertyName;
+import org.apache.jackrabbit.webdav.property.ResourceType;
+import org.apache.jackrabbit.webdav.xml.Namespace;
+
+/**
+ * Provides constants for media types, XML namespaces, names and
+ * values, DAV properties and resource types defined by the WebDAV ACL
+ * spec.
+ */
+public interface AclConstants extends DavConstants {
+
+    /** The ACL XML element name <DAV:principal> */
+    public static final String ELEMENT_ACL_PRINCIPAL = "principal";
+    /** The ACL XML element name <DAV:alternate-URI-set> */
+    public static final String ELEMENT_ACL_ALTERNATE_URI_SET =
+        "alternate-URI-set";
+
+    /** The ACL property name DAV:alternate-URI-set */
+    public static final String PROPERTY_ACL_ALTERNATE_URI_SET =
+        "alternate-URI-set";
+
+    /** The ACL property DAV:alternate-URI-set */
+    public static final DavPropertyName ALTERNATEURISET =
+        DavPropertyName.create(PROPERTY_ACL_ALTERNATE_URI_SET,
+                               NAMESPACE);
+}
---
+++ src/main/java/org/osaf/cosmo/dav/acl/property/AlternateUriSet.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2005-2006 Open Source Applications Foundation
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.osaf.cosmo.dav.acl.property;
+
+import org.apache.jackrabbit.webdav.property.AbstractDavProperty;
+import org.apache.jackrabbit.webdav.xml.DomUtil;
+import org.apache.jackrabbit.webdav.xml.Namespace;
+import org.apache.jackrabbit.webdav.xml.XmlSerializable;
+
+import org.osaf.cosmo.CosmoConstants;
+import org.osaf.cosmo.dav.acl.AclConstants;
+import org.osaf.cosmo.dav.impl.DavHomeCollection;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Document;
+
+/**
+ * Represents the DAV:alternate-URI-set property.
+ *
+ * This property is protected. The value contains three DAV:href
+ * elements specifying the Atom, CMP and web URLs for the principal.
+ */
+public class AlternateUriSet extends AbstractDavProperty
+    implements AclConstants {
+
+    private DavHomeCollection home;
+
+    /**
+     */
+    public AlternateUriSet(DavHomeCollection home) {
+        super(ALTERNATEURISET, true);
+        this.home = home;
+    }
+
+    /**
+     * Returns a
+     * <code>AlternateUriSet.AlternateUriSetInfo</code>
+     * for this property.
+     */
+    public Object getValue() {
+        return new AlternateUriSetInfo();
+    }
+
+    /**
+     */
+    public class AlternateUriSetInfo implements XmlSerializable {
+  
+        /**
+         */
+        public Element toXml(Document document) {
+            Element href =
+                DomUtil.createElement(document, XML_HREF, NAMESPACE);
+            DomUtil.setText(href, home.getLocator().getHref(true));
+
+            Element set =
+                DomUtil.createElement(document,
+                                      ELEMENT_ACL_ALTERNATE_URI_SET,
+                                      NAMESPACE);
+            set.appendChild(href);
+
+            return set;
+        }
+    }
+}

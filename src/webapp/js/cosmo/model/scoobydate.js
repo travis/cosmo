84b1f05f0a6942698e750f8c2cb8aeeeec556c2b
mde <mde@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2006-10-06 00:20:04 +0000

    Cleaned up indenting, put setters and getters into descending order of time-unit size.
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@2619 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- src/webapp/js/cosmo/model/scoobydate.js
+++ src/webapp/js/cosmo/model/scoobydate.js
@@ -37,223 +37,222 @@ function ScoobyDate(year, month, date, hours, minutes, seconds, timezone, utc) {
     this.timezone = timezone ? timezone : null;
     this.utc = utc ? utc : false;
 }
-    ScoobyDate.prototype.getDate = function() {
-        return this.date;
-    };
-    
-    ScoobyDate.prototype.getFullYear = function() {
-        return this.getYear();
-    };
-    
-    ScoobyDate.prototype.getHours = function() {
-        return this.hours;
-    };
-    ScoobyDate.prototype.getMinutes = function() {
-        return this.minutes;
-    };
-    ScoobyDate.prototype.getMonth = function() {
-        return this.month;
-    };
-    ScoobyDate.prototype.getSeconds = function() {
-        return this.seconds;
-    };
-    ScoobyDate.prototype.getYear = function() {
-        return this.year;
-    };
-    ScoobyDate.prototype.setDate = function(dat) {
-        this.date = dat;
-    };
-    ScoobyDate.prototype.setFullYear = function(yea) {
-        this.setYear(yea);
-    };
-    ScoobyDate.prototype.setHours = function(hou, min) {
-        this.hours = hou;
-        if (min) {
-            this.setMinutes(min);
-        }
-    };
-    ScoobyDate.prototype.setMinutes = function(min) {
-        this.minutes = min;
-    };
-    ScoobyDate.prototype.setMonth = function(mon) {
-        this.month = mon;
-    };
-    ScoobyDate.prototype.setSeconds = function(sec) {
-        this.seconds = sec;
-    };
-    ScoobyDate.prototype.setYear = function(yea) {
-        this.year = yea;
-    };
-    /**
-     * Returns the time in milliseconds since January 1st, 1970 UTC.
-     */
-    ScoobyDate.prototype.toUTC = function() {
-        var utc = Date.UTC(this.getYear(), this.getMonth(), this.getDate(),
-            this.getHours(), this.getMinutes(), this.getSeconds());
-        return(utc - this.getTimezoneOffsetMs());
-    };
-    /**
-     * Updates a ScoobyDate based on a UTC stamp
-     * Naive implementation assumes timezone offset for new UTC does not change
-     */
-    ScoobyDate.prototype.updateFromUTC = function(utc) {
-        var dt = null;
-        // Get a fake Date object in UTC frame of reference
-        dt = new Date(utc + this.getTimezoneOffsetMs());
-        // Update ScoobyDate values based on UTC values
-        this.year = dt.getUTCFullYear();
-        this.month = dt.getUTCMonth();
-        this.date = dt.getUTCDate();
-        this.hours = dt.getUTCHours();
-        this.minutes = dt.getUTCMinutes();
-        this.seconds = dt.getUTCSeconds();
-        // Update TZ if needed
-        if (this.timezone) {
-            this.timezone = ScoobyDate.getNewTimezoneForUTC(this.timezone, utc);
-        }
-    };
-    /**
-     * Updates a ScoobyDate based on a values from a local JS Date
-     * Naive implementation assumes timezone offset for new UTC does not change
-     * BANDAID: NEEDS TO BE REFACTORED
-     */
-    ScoobyDate.prototype.updateFromLocalDate = function(dt) {
-        // Update ScoobyDate values based on local JS date values
-        // Used to make changes to dates that are already in a specific timezone
-        this.year = dt.getFullYear();
-        this.month = dt.getMonth();
-        this.date = dt.getDate();
-        this.hours = dt.getHours();
-        this.minutes = dt.getMinutes();
-        this.seconds = dt.getSeconds();
-    };
-    /**
-     * Returns the offset from GMT in minutes
-     * Minus GMT results sensibly in negative values -- this is opposite
-     * behavior from JavaScript's Date object
-     */
-    ScoobyDate.prototype.getTimezoneOffset = function() {
-        var offsetMin = 0;
 
-        // Is UTC, no need to do more work
-        if (this.utc) {
-            offsetMin = 0;
-        }
-        // Has timezone, use offset from timezone
-        if (this.timezone) {
-            offsetMin = this.timezone.minutesOffset;
-        }
-        // No timezone, no UTC -- must be a floating date
-        else {
-            offsetMin = this.getUserPrefTimezoneOffset();
-        }
-        return offsetMin;
-    };
-    /**
-     * Returns the offset from GMT in milliseconds
-     */
-    ScoobyDate.prototype.getTimezoneOffsetMs = function() {
-        return(this.getTimezoneOffset()*60*1000);
-    };
-    /**
-      * One place to go to get the user-pref timezone offset for a ScoobyDate
-      * This should ultimately work with the Scooby app's user prefs
-      * Or independently with fallback to the normal browser local offset
-      */
-    ScoobyDate.prototype.getUserPrefTimezoneOffset = function() {
-        var offsetMin = 0;
-        // Try to look up user pref
-        if (typeof Pref == 'object') {
-            offsetMin = Pref.getTimezoneOffset(this);
-        }
-        // Otherwise punt and go with the browser's offset for that date
-        else {
-            offsetMin = ScoobyDate.getBrowserTimezoneOffset(
-                this.getYear(), this.getMonth(), this.getDate(),
-                this.getHours(), this.getMinutes(), this.getSeconds());
-        }
-        return offsetMin;
-    };
-    /**
-     * Formats the date in it's original timezone, using the strftime function
-     * found in date.js.
-     *
-     * For example, if the timezone of the ScoobyDate is EST and it's
-     * Date is "1/2/2006 19:30", than that is the date that this function will
-     * return no matter what your local timezone is.
-     */
-    ScoobyDate.prototype.strftime = function(formatString){
-        // No need to do any mucking around with UTC offsets or anything
-        // for this function, since all we care about is the output
-        var d = new Date(this.getYear(), this.getMonth(), this.getDate(),
+ScoobyDate.prototype.getFullYear = function() {
+    return this.getYear();
+};
+ScoobyDate.prototype.getYear = function() {
+    return this.year;
+};
+ScoobyDate.prototype.getMonth = function() {
+    return this.month;
+};
+ScoobyDate.prototype.getDate = function() {
+    return this.date;
+};
+ScoobyDate.prototype.getHours = function() {
+    return this.hours;
+};
+ScoobyDate.prototype.getMinutes = function() {
+    return this.minutes;
+};
+ScoobyDate.prototype.getSeconds = function() {
+    return this.seconds;
+};
+ScoobyDate.prototype.setFullYear = function(yea) {
+    this.setYear(yea);
+};
+ScoobyDate.prototype.setYear = function(yea) {
+    this.year = yea;
+};
+ScoobyDate.prototype.setMonth = function(mon) {
+    this.month = mon;
+};
+ScoobyDate.prototype.setDate = function(dat) {
+    this.date = dat;
+};
+ScoobyDate.prototype.setHours = function(hou, min) {
+    this.hours = hou;
+    if (min) {
+        this.setMinutes(min);
+    }
+};
+ScoobyDate.prototype.setMinutes = function(min) {
+    this.minutes = min;
+};
+ScoobyDate.prototype.setSeconds = function(sec) {
+    this.seconds = sec;
+};
+/**
+ * Returns the time in milliseconds since January 1st, 1970 UTC.
+ */
+ScoobyDate.prototype.toUTC = function() {
+    var utc = Date.UTC(this.getYear(), this.getMonth(), this.getDate(),
+        this.getHours(), this.getMinutes(), this.getSeconds());
+    return(utc - this.getTimezoneOffsetMs());
+};
+/**
+ * Updates a ScoobyDate based on a UTC stamp
+ * Naive implementation assumes timezone offset for new UTC does not change
+ */
+ScoobyDate.prototype.updateFromUTC = function(utc) {
+    var dt = null;
+    // Get a fake Date object in UTC frame of reference
+    dt = new Date(utc + this.getTimezoneOffsetMs());
+    // Update ScoobyDate values based on UTC values
+    this.year = dt.getUTCFullYear();
+    this.month = dt.getUTCMonth();
+    this.date = dt.getUTCDate();
+    this.hours = dt.getUTCHours();
+    this.minutes = dt.getUTCMinutes();
+    this.seconds = dt.getUTCSeconds();
+    // Update TZ if needed
+    if (this.timezone) {
+        this.timezone = ScoobyDate.getNewTimezoneForUTC(this.timezone, utc);
+    }
+};
+/**
+ * Updates a ScoobyDate based on a values from a local JS Date
+ * Naive implementation assumes timezone offset for new UTC does not change
+ * BANDAID: NEEDS TO BE REFACTORED
+ */
+ScoobyDate.prototype.updateFromLocalDate = function(dt) {
+    // Update ScoobyDate values based on local JS date values
+    // Used to make changes to dates that are already in a specific timezone
+    this.year = dt.getFullYear();
+    this.month = dt.getMonth();
+    this.date = dt.getDate();
+    this.hours = dt.getHours();
+    this.minutes = dt.getMinutes();
+    this.seconds = dt.getSeconds();
+};
+/**
+ * Returns the offset from GMT in minutes
+ * Minus GMT results sensibly in negative values -- this is opposite
+ * behavior from JavaScript's Date object
+ */
+ScoobyDate.prototype.getTimezoneOffset = function() {
+    var offsetMin = 0;
+
+    // Is UTC, no need to do more work
+    if (this.utc) {
+        offsetMin = 0;
+    }
+    // Has timezone, use offset from timezone
+    if (this.timezone) {
+        offsetMin = this.timezone.minutesOffset;
+    }
+    // No timezone, no UTC -- must be a floating date
+    else {
+        offsetMin = this.getUserPrefTimezoneOffset();
+    }
+    return offsetMin;
+};
+/**
+ * Returns the offset from GMT in milliseconds
+ */
+ScoobyDate.prototype.getTimezoneOffsetMs = function() {
+    return(this.getTimezoneOffset()*60*1000);
+};
+/**
+  * One place to go to get the user-pref timezone offset for a ScoobyDate
+  * This should ultimately work with the Scooby app's user prefs
+  * Or independently with fallback to the normal browser local offset
+  */
+ScoobyDate.prototype.getUserPrefTimezoneOffset = function() {
+    var offsetMin = 0;
+    // Try to look up user pref
+    if (typeof Pref == 'object') {
+        offsetMin = Pref.getTimezoneOffset(this);
+    }
+    // Otherwise punt and go with the browser's offset for that date
+    else {
+        offsetMin = ScoobyDate.getBrowserTimezoneOffset(
+            this.getYear(), this.getMonth(), this.getDate(),
             this.getHours(), this.getMinutes(), this.getSeconds());
-        return Date.strftime(formatString, d);
-    };
-    /**
-     * Increments by the desired number of specified units
-     */
-    ScoobyDate.prototype.add = function(interv, incr) {
-        var dt = null;
-        var ret = null;
-        // Get incremented localtime JS Date obj
-        dt = Date.add(interv, incr, this.toUTC());
-        // Update this date based on the new UTC
-        this.updateFromUTC(dt.getTime());
-    };
-    /**
-     * Formats the ScoobyDate according to what time the date falls on in the user's
-     * local timezone.
-     *
-     * For example, if this ScoobyDate represents the date "1/1/2006 12am PST"
-     * and your local timezone is EST, the date will be formatted as "1/1/2006 3am"
-     */
-    ScoobyDate.prototype.strftimeLocalTimezone = function(formatString){
-            var localScoobyDate = this.createLocalScoobyDate();
-            return localScoobyDate.strftime(formatString);
-    };
-    /**
-     * Returns what the ScoobyDate representing the same point in time as this ScoobyDate,
-     * but in the user's local timezone.
-     *
-     * For example, if this ScoobyDate represents the date "1/1/2006 12am PST"
-     * and your local timezone is EST, the returned date will be "1/1/2006 3am EST"
-     */
-    ScoobyDate.prototype.createLocalScoobyDate = function(){
-        //For now, we can cheat using new Date() since that gives us a local date
-        //with the given UTC
-        var utc = this.toUTC();
-        var dt = new Date(utc);
-        var tz = ScoobyTimezone.getLocalTimezone(utc);
-        var scoobDt = new ScoobyDate(dt.getFullYear(), dt.getMonth(),
-            dt.getDate(), dt.getHours(), dt.getMinutes(),
-            dt.getSeconds(), tz, false);
-        return scoobDt;
-    };
-    /**
-     * Returns the day of the week occupied by a ScoobyDate
-     * but in the user's local timezone.
-     */
-    ScoobyDate.prototype.getLocalDay = function() {
-        var localDt = new Date(this.toUTC());
-        return localDt.getDay();
-    };
-    ScoobyDate.prototype.after = function(dt){
-        var utc = (typeof dt == "number") ? dt : dt.toUTC();
-        return  this.toUTC() > utc;
-    };
-    ScoobyDate.prototype.before = function(dt){
-        var utc = (typeof dt == "number") ? dt : dt.toUTC();
-        return  this.toUTC() < utc;
-    };
-    /**
-     * Returns UTC timestamp for a ScoobyDate
-     * Leave for now for API compatibility
-     */
-    ScoobyDate.prototype.getTime = function() {
-       return this.toUTC();
-    };
-    
-    ScoobyDate.prototype.toString = genericToString;
+    }
+    return offsetMin;
+};
+/**
+ * Formats the date in it's original timezone, using the strftime function
+ * found in date.js.
+ *
+ * For example, if the timezone of the ScoobyDate is EST and it's
+ * Date is "1/2/2006 19:30", than that is the date that this function will
+ * return no matter what your local timezone is.
+ */
+ScoobyDate.prototype.strftime = function(formatString){
+    // No need to do any mucking around with UTC offsets or anything
+    // for this function, since all we care about is the output
+    var d = new Date(this.getYear(), this.getMonth(), this.getDate(),
+        this.getHours(), this.getMinutes(), this.getSeconds());
+    return Date.strftime(formatString, d);
+};
+/**
+ * Increments by the desired number of specified units
+ */
+ScoobyDate.prototype.add = function(interv, incr) {
+    var dt = null;
+    var ret = null;
+    // Get incremented localtime JS Date obj
+    dt = Date.add(interv, incr, this.toUTC());
+    // Update this date based on the new UTC
+    this.updateFromUTC(dt.getTime());
+};
+/**
+ * Formats the ScoobyDate according to what time the date falls on in the user's
+ * local timezone.
+ *
+ * For example, if this ScoobyDate represents the date "1/1/2006 12am PST"
+ * and your local timezone is EST, the date will be formatted as "1/1/2006 3am"
+ */
+ScoobyDate.prototype.strftimeLocalTimezone = function(formatString){
+        var localScoobyDate = this.createLocalScoobyDate();
+        return localScoobyDate.strftime(formatString);
+};
+/**
+ * Returns what the ScoobyDate representing the same point in time as this ScoobyDate,
+ * but in the user's local timezone.
+ *
+ * For example, if this ScoobyDate represents the date "1/1/2006 12am PST"
+ * and your local timezone is EST, the returned date will be "1/1/2006 3am EST"
+ */
+ScoobyDate.prototype.createLocalScoobyDate = function(){
+    //For now, we can cheat using new Date() since that gives us a local date
+    //with the given UTC
+    var utc = this.toUTC();
+    var dt = new Date(utc);
+    var tz = ScoobyTimezone.getLocalTimezone(utc);
+    var scoobDt = new ScoobyDate(dt.getFullYear(), dt.getMonth(),
+        dt.getDate(), dt.getHours(), dt.getMinutes(),
+        dt.getSeconds(), tz, false);
+    return scoobDt;
+};
+/**
+ * Returns the day of the week occupied by a ScoobyDate
+ * but in the user's local timezone.
+ */
+ScoobyDate.prototype.getLocalDay = function() {
+    var localDt = new Date(this.toUTC());
+    return localDt.getDay();
+};
+ScoobyDate.prototype.after = function(dt){
+    var utc = (typeof dt == "number") ? dt : dt.toUTC();
+    return  this.toUTC() > utc;
+};
+ScoobyDate.prototype.before = function(dt){
+    var utc = (typeof dt == "number") ? dt : dt.toUTC();
+    return  this.toUTC() < utc;
+};
+/**
+ * Returns UTC timestamp for a ScoobyDate
+ * Leave for now for API compatibility
+ */
+ScoobyDate.prototype.getTime = function() {
+   return this.toUTC();
+};
+
+ScoobyDate.prototype.toString = genericToString;
 
 
 // ScoobyDate static methods

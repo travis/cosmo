72b52ebd9e138852e4501fe94796d286f73272b0
travis <travis@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2006-11-06 18:19:15 +0000

    Fixed package name.
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@2823 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- src/main/java/org/osaf/cosmo/spring/mvc/controllers/CosmoUrlFilenameViewController.java
+++ src/main/java/org/osaf/cosmo/spring/mvc/controllers/CosmoUrlFilenameViewController.java
@@ -13,10 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.osaf.cosmo.spring.mvc;
+package org.osaf.cosmo.spring.mvc.controllers;
 
 import org.springframework.web.servlet.mvc.UrlFilenameViewController;
-import org.springframework.web.util.WebUtils;
 
 public class CosmoUrlFilenameViewController extends UrlFilenameViewController {
 	

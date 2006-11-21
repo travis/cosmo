69dd133ab9d4207dff0acb391709d35a7d6ac8af
travis <travis@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2006-11-21 00:48:30 +0000

    Button.html specified an id of "savebuttontext" for its text div, making it impossible to give it a context-specific id. This has been changed so that the text div will have an id of
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@2896 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- src/webapp/js/cosmo/ui/widget/LoginDialog.js
+++ src/webapp/js/cosmo/ui/widget/LoginDialog.js
@@ -123,7 +123,12 @@ dojo.widget.defineWidget("cosmo.ui.widget.LoginDialog", dojo.widget.HtmlWidget,
 
 			// Programmatic subwidget creation should be removed once safari supports
 			// widgetsInTemplate
-			var button = dojo.widget.createWidget("cosmo:Button", { text:_("Login.Button.Ok"), width:74 });
+			var button = dojo.widget.createWidget("cosmo:Button",
+					{ 	text: _("Login.Button.Ok"),
+						width: 74,
+						widgetId: "loginSubmitButton"
+
+					});
 
 			dojo.dom.prependChild(button.domNode, this.submitButton.parentNode);
 			dojo.dom.removeNode(this.submitButton);

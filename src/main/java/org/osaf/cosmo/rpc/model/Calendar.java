85513f01611ffeca4d314ddd97f9a89d7b0db1a3
br <br@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2006-12-11 23:05:47 +0000

    formatting changes - removed spurious whitespace.
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@3056 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- src/main/java/org/osaf/cosmo/rpc/model/Calendar.java
+++ src/main/java/org/osaf/cosmo/rpc/model/Calendar.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2006 Open Source Applications Foundation
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,49 +15,41 @@
  */
 package org.osaf.cosmo.rpc.model;
 
+import java.util.Map;
+
 
 /**
  * This class contains information about a user's calendar.
- * 
+ *
  * @author bobbyrullo
  */
 public class Calendar {
-	
+
     private String displayName = null;
-    private String path = null;
     private String uid = null;
-    
+
+
     /**
-     * This is the name as displayed to the user 
+     * This is the name as displayed to the user
      */
     public String getName() {
         return displayName;
     }
-    
+
     public void setName(String displayName) {
         this.displayName = displayName;
     }
-    
-    /**
-     * This is the path to the calendar collection, relative to the user's 
-     * home directory
-     */
-    public String getPath() {
-        return path;
-    }
-    public void setPath(String path) {
-        this.path = path;
-    }
-    
+
+
     /**
      * This is the uid of this collection.
      */
     public String getUid() {
         return uid;
     }
-    
+
     public void setUid(String uid) {
         this.uid = uid;
     }
-    
+
 }

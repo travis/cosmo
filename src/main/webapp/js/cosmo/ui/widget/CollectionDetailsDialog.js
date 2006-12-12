2e39f97260228ef196e25235b2efbe1b035ebc48
br <br@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2006-12-12 23:17:08 +0000

    Almost finished work on bug 7374, CollectionDetails dialog box, with drop down menu, but we are trying another approach, probably. Still wanted to get this into svn in case we do something similar, and need to revert back to it :-)
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@3077 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- src/main/webapp/js/cosmo/ui/widget/CollectionDetailsDialog.js
+++ src/main/webapp/js/cosmo/ui/widget/CollectionDetailsDialog.js
@@ -14,49 +14,103 @@
  * limitations under the License.
 */
 
-/**
- * @fileoverview ModalDialog -- modal dialog, including full-window
- * masking div to prevent user from interacting with underlying
- * doc when dialog is showing. Content area can be a string of
- * HTML or a DOM node to insert. If the title property is not
- * empty, the dialog has a rectangular title bar at the top.
- * the defaultAction prop should be a function object to be
- * executed by default from the Enter key if the dialog is being
- * displayed.
- * @author Bobby Rullo br@osafoundation.org
- * @license Apache License 2.0
- */
-
 dojo.provide("cosmo.ui.widget.CollectionDetailsDialog");
 
 dojo.require("dojo.widget.*");
 dojo.require("dojo.html.common");
+
 dojo.require("cosmo.env");
+dojo.require("cosmo.app");
 dojo.require("cosmo.util.i18n");
+dojo.require("cosmo.util.html");
 dojo.require("cosmo.ui.widget.Button");
 dojo.require("cosmo.ui.widget.ModalDialog");
 
-dojo.widget.defineWidget("cosmo.ui.widget.CollectionDetailsDialog",
-dojo.widget.HtmlWidget, {
+dojo.widget.defineWidget("cosmo.ui.widget.CollectionDetailsDialog", "html",
+
+//init function.
+dojo.widget.HtmlWidget, function(){
+
+}
+,
+//Prototype Properties 
+{
+
+        //User supplied properties:
+        calendar: /*CalendarMetadata*/ null,
+        
         // Template stuff
         templatePath:dojo.uri.dojoUri(
             '../../cosmo/ui/widget/templates/CollectionDetailsDialog/CollectionDetailsDialog.html'),
 
         // Attach points
-
+        clientSelector: null, //Selector for different client apps
+        clientInstructions: null, //Instructions for the selected client
+        protocolLink: null, //The link for the selected client
+        instructionsBlock: null, //The instructions, link for the selected client
+        
         // Instance methods
 
         
         //i18n
         strings: { 
             nameLabel: getText("Main.CollectionDetails.NameLabel"),
+            calendarLabel: getText("Main.CollectionDetails.CalendarLabel"),
+            selectYourClient: getText("Main.CollectionDetails.SelectYourClient"),
             collectionAddress: getText("Main.CollectionDetails.CollectionAddress"),
             instructions1: getText("Main.CollectionDetails.Instructions.1"),
             instructions2: getText("Main.CollectionDetails.Instructions.2"),
             close: getText("Main.CollectionDetails.Close")
-            
+        },
+        
+        //clients - note: the order in which they appear here is the order in
+        //                they will appear in the select box
+        clients: ["Chandler", "iCal", "Evolution", "Outlook", "Sunbird" ],
+        
+        clientsToProtocols: {
+            Chandler: "mc",
+            iCal: "webcal",
+            Evolution: "dav",
+            Outlook: "webcal",
+            Sunbird: "dav"
         },
 
         // Lifecycle functions
-        /*postMixInProperties: function () { }*/ }
+        fillInTemplate: function(){
+            //set up the select box
+            var options = [{value: "", text: ""}];
+            dojo.lang.map(this.clients,function(client){
+                options.push({
+                    value:client,
+                    text: getText("Main.CollectionDetails.Client." + client)
+                })
+            });
+          cosmo.util.html.setSelectOptions(this.clientSelector, options);
+        },
+        
+        //handles when the user selects a client
+        _handleClientChanged: function(){
+            this.instructionsBlock.style.display = "block";
+            var selectedIndex = this.clientSelector.selectedIndex;
+            var client = this.clientSelector.options[selectedIndex].value;
+            dojo.dom.replaceChildren(this.clientInstructions, document.createTextNode(getText("Main.CollectionDetails.Instructions." + client)));
+            var url =  this.calendar.protocolUrls[this.clientsToProtocols[client]];
+            this.protocolLink.href = url;
+            dojo.dom.replaceChildren(this.protocolLink, document.createTextNode(url));
+        }
+ }
  );
+ 
+ cosmo.ui.widget.CollectionDetailsDialog.getInitProperties = function(/*CalendarMetadata*/ calendar){
+    return {
+        content: dojo.widget.createWidget("cosmo:CollectionDetailsDialog", {calendar: calendar}, null, 'last'),
+        width: "400",
+        btnsRight: [dojo.widget.createWidget(
+                    "cosmo:Button",
+                    { text: getText("Main.CollectionDetails.Close"),
+                      width: "50px",
+                      handleOnClick: cosmo.app.hideDialog,
+                      small: false },
+                      null, 'last')]
+    };
+ };

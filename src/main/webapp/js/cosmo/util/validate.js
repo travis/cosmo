e38b9b5e0907a84589955f8c4309b1500ee428bb
mde <mde@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2007-01-19 00:36:15 +0000

    Remove superfluous newlines at the end of error message returned by date format validator. Updated standard validators to accept either the form element or the value itself.
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@3394 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- src/main/webapp/js/cosmo/util/validate.js
+++ src/main/webapp/js/cosmo/util/validate.js
@@ -32,7 +32,7 @@ cosmo.util.validate = new function () {
         // Check format
         var matchArray = str.match(pat);
         if (!matchArray) {
-            errMsg += _('App.Error.InvalidDateFormat') + '\n';
+            errMsg += _('App.Error.InvalidDateFormat');
         }
         else {
             // Parse date parts into vars
@@ -41,11 +41,11 @@ cosmo.util.validate = new function () {
             year = matchArray[4];
             // Month range
             if (month < 1 || month > 12) {
-                errMsg += _('App.Error.InvalidMonthRange') + '\n';
+                errMsg += _('App.Error.InvalidMonthRange');
             }
             // Day range
             if (day < 1 || day > 31) {
-                errMsg += _('App.Error.InvalidDayRange') + '\n';
+                errMsg += _('App.Error.InvalidDayRange');
             }
             // Day 31 for correct months
             if ((month == 4 || month == 6 || month == 9 || month == 11) 
@@ -56,7 +56,7 @@ cosmo.util.validate = new function () {
             if (month == 2) {
                 var isLeap = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
                 if (day > 29 || (day == 29 && !isLeap)) {
-                    errMsg += _('App.Error.FebruaryDays') + '\n';
+                    errMsg += _('App.Error.FebruaryDays');
                 }
             }
         }
@@ -69,16 +69,16 @@ cosmo.util.validate = new function () {
         // Check format
         var matchArray = str.match(pat);
         if (!matchArray) {
-            errMsg += _('App.Error.InvalidTimeFormat') + '\n';
+            errMsg += _('App.Error.InvalidTimeFormat');
         }
         else {
             hours = matchArray[1];
             minutes = matchArray[3];
             if (hours < 1 || hours > 12) {
-                errMsg += _('App.Error.InvalidHourRange') + '\n';
+                errMsg += _('App.Error.InvalidHourRange');
             }
             if (minutes < 0 || minutes > 59) {
-                errMsg += _('App.Error.InvalidMinutesRange') + '\n';
+                errMsg += _('App.Error.InvalidMinutesRange');
             }
         }
         return errMsg;
@@ -87,9 +87,9 @@ cosmo.util.validate = new function () {
      * Makes sure the given text input has a given length
      * @return String, error message (empty if no err).
      */
-    this.minLength = function (elem, len) {
+    this.minLength = function (s, len) {
         err = '';
-        val = elem.value;
+        val = typeof s == 'object' ? s.value : s;
         // Only bother checking length if a value is present
         // Requiring a value should be done with 'required' method
         if (val && (val.length < len)) {
@@ -101,9 +101,9 @@ cosmo.util.validate = new function () {
      * Makes sure the given text input is not empty
      * @return String, error message (empty if no err).
      */
-    this.required = function (elem) {
+    this.required = function (s) {
         err = '';
-        val = elem.value;
+        val = (s != null && typeof s == 'object') ? s.value : s;
         if (!val) {
             err = _('Signup.Error.RequiredField');
         }
@@ -113,11 +113,11 @@ cosmo.util.validate = new function () {
      * Makes sure the given text input is a valid e-mail address
      * @return String, error message (empty if no err).
      */
-    this.eMail = function (elem) {
+    this.eMail = function (s) {
         // Just do really basic e-mail addr validation
         pat = /^.+@.+\..{2,3}$/;
         err = '';
-        val = elem.value;
+        val = typeof s == 'object' ? s.value : s;
         if (!pat.test(val)) {
             err = _('Signup.Error.ValidEMail');
         }
@@ -127,10 +127,10 @@ cosmo.util.validate = new function () {
      * Makes sure the given password field matches the other
      * @return String, error message (empty if no err).
      */
-    this.confirmPass = function (elem, elemCompare) {
+    this.confirmPass = function (s, sCompare) {
         err = '';
-        val = elem.value;
-        val2 = elemCompare.value;
+        val = typeof s == 'object' ? s.value : s;
+        val2 = typeof sCompare == 'object' ? sCompare.value : s;
         if (val != val2) {
             err = _('Signup.Error.MatchPassword');
         }

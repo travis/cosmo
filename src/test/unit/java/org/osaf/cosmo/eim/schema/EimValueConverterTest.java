a5825de508ec5a2a33caa137d76a0b2b14b3bdc7
bcm <bcm@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2007-01-23 23:22:50 +0000

    add tests for eim text <-> icalendar date/datetime conversion
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@3444 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- src/test/unit/java/org/osaf/cosmo/eim/schema/EimValueConverterTest.java
+++ src/test/unit/java/org/osaf/cosmo/eim/schema/EimValueConverterTest.java
@@ -18,6 +18,13 @@ package org.osaf.cosmo.eim.schema;
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
+import net.fortuna.ical4j.model.Date;
+import net.fortuna.ical4j.model.DateList;
+import net.fortuna.ical4j.model.DateTime;
+import net.fortuna.ical4j.model.TimeZone;
+import net.fortuna.ical4j.model.TimeZoneRegistry;
+import net.fortuna.ical4j.model.TimeZoneRegistryFactory;
+import net.fortuna.ical4j.model.parameter.Value;
 import net.fortuna.ical4j.model.property.Trigger;
 
 import org.apache.commons.logging.Log;
@@ -26,6 +33,159 @@ import org.apache.commons.logging.LogFactory;
 public class EimValueConverterTest extends TestCase {
     private static final Log log =
         LogFactory.getLog(EimValueConverterTest.class);
+    private static final TimeZoneRegistry TIMEZONE_REGISTRY =
+        TimeZoneRegistryFactory.getInstance().createRegistry();
+
+    public void testToICalDatesAsDateTime() throws Exception {
+        String str = "VALUE=DATE-TIME:TZID=America/Los_Angeles:20021010T120000";
+
+        DateList dl = EimValueConverter.toICalDates(str);
+        assertNotNull("null date list", dl);
+        assertEquals("wrong number of dates", 1, dl.size());
+
+        TimeZone tz = dl.getTimeZone();
+        assertNotNull("null timezone", tz);
+        assertEquals("incorrect timezone", "America/Los_Angeles", tz.getID());
+
+        assertEquals("not datetime", Value.DATE_TIME, dl.getType());
+        DateTime dt = (DateTime) dl.get(0);
+        assertEquals("incorrect datetime", "20021010T120000", dt.toString());
+
+        TimeZone dtTz = dt.getTimeZone();
+        assertNotNull("null datetime timezone", dtTz);
+        assertEquals("incorrect datetime timezone", "America/Los_Angeles",
+                     dtTz.getID());
+    }
+    
+    public void testToICalDatesAsDate() throws Exception {
+        String str = "VALUE=DATE:20021010T";
+
+        DateList dl = EimValueConverter.toICalDates(str);
+        assertNotNull("null date list", dl);
+        assertEquals("wrong number of dates", 1, dl.size());
+
+        assertNull("not null timezone", dl.getTimeZone());
+        assertEquals("not date", Value.DATE, dl.getType());
+
+        assertEquals("incorrect date", "20021010", dl.get(0).toString());
+    }
+    
+    public void testToICalDatesMultiple() throws Exception {
+        String str = "VALUE=DATE:20021010,20021011,20021012";
+
+        DateList dl = EimValueConverter.toICalDates(str);
+        assertNotNull("null date list", dl);
+        assertEquals("wrong number of dates", 3, dl.size());
+
+        assertNull("not null timezone", dl.getTimeZone());
+
+        assertEquals("not date", Value.DATE, dl.getType());
+
+        assertEquals("incorrect date", "20021010", dl.get(0).toString());
+        assertEquals("incorrect date", "20021011", dl.get(1).toString());
+        assertEquals("incorrect date", "20021012", dl.get(2).toString());
+    }
+    
+    public void testToICalDateAsDateTime() throws Exception {
+        String str = "VALUE=DATE-TIME:TZID=America/Los_Angeles:20021010T120000";
+
+        Date d = EimValueConverter.toICalDate(str);
+        assertNotNull("null date", d);
+        assertTrue("not datetime", d instanceof DateTime);
+
+        DateTime dt = (DateTime) d;
+        assertEquals("incorrect datetime", "20021010T120000", dt.toString());
+
+        TimeZone tz = dt.getTimeZone();
+        assertNotNull("null timezone", tz);
+        assertEquals("incorrect timezone", "America/Los_Angeles", tz.getID());
+    }
+    
+    public void testToICalDateAsDate() throws Exception {
+        String str = "VALUE=DATE:20021010";
+
+        Date d = EimValueConverter.toICalDate(str);
+        assertNotNull("null date", d);
+        assertFalse("not date", d instanceof DateTime);
+        assertEquals("incorrect date", "20021010", d.toString());
+    }
+    
+    public void testToICalDateNoParams() throws Exception {
+        DateList dl = EimValueConverter.toICalDates("20021010T120000");
+        assertEquals("not datetime", Value.DATE_TIME, dl.getType());
+        assertNull("timezone where none should be", dl.getTimeZone());
+    }
+
+    public void testToICalDateParseErrors() throws Exception {
+        try {
+            EimValueConverter.toICalDate("FOO=bar:20021010T120000");
+            fail("converted with bad param FOO");
+        } catch (EimConversionException e) {}
+
+        try {
+            EimValueConverter.toICalDate("TZID=deadbeef:20021010T120000");
+            fail("converted with bad TZID deadbeef");
+        } catch (EimConversionException e) {}
+
+        try {
+            EimValueConverter.toICalDate("VALUE=deadbeef:20021010T120000");
+            fail("converted with bad VALUE deadbeef");
+        } catch (EimConversionException e) {}
+
+        try {
+            EimValueConverter.toICalDate("deadbeef");
+            fail("converted with bad text deadbeef");
+        } catch (EimConversionException e) {}
+    }
+    
+    public void testFromICalDatesAsDateTime() throws Exception {
+        TimeZone tz = TIMEZONE_REGISTRY.getTimeZone("America/Los_Angeles");
+        DateList dl = new DateList(Value.DATE_TIME, tz);
+        dl.add(new DateTime("20021010T120000", tz));
+
+        String test = "VALUE=DATE-TIME:TZID=America/Los_Angeles:20021010T120000";
+        String result = EimValueConverter.fromICalDates(dl);
+        assertNotNull("null result", result);
+        assertEquals(test, result);
+    }
+
+    public void testFromICalDatesAsDate() throws Exception {
+        DateList dl = new DateList(Value.DATE);
+        dl.add(new Date("20021010"));
+
+        String test = "VALUE=DATE:20021010";
+        String result = EimValueConverter.fromICalDates(dl);
+        assertNotNull("null result", result);
+        assertEquals(test, result);
+    }
+
+    public void testFromICalDatesMultiple() throws Exception {
+        DateList dl = new DateList(Value.DATE);
+        dl.add(new Date("20021010"));
+        dl.add(new Date("20021011"));
+        dl.add(new Date("20021012"));
+
+        String str = "VALUE=DATE:20021010,20021011,20021012";
+    }
+    
+    public void testFromICalDateAsDateTime() throws Exception {
+        TimeZone tz = TIMEZONE_REGISTRY.getTimeZone("America/Los_Angeles");
+        DateTime dt = new DateTime("20021010T120000", tz);
+
+        String test = "VALUE=DATE-TIME:TZID=America/Los_Angeles:20021010T120000";
+        String result = EimValueConverter.fromICalDate(dt);
+        assertNotNull("null result", result);
+        assertEquals(test, result);
+    }
+    
+    public void testFromICalDateAsDate() throws Exception {
+        Date d = new Date("20021010");
+
+        String test = "VALUE=DATE:20021010";
+        String result = EimValueConverter.fromICalDate(d);
+        assertNotNull("null result", result);
+        assertEquals(test, result);
+    }
     
     public void testToIcalTrigger() throws Exception {
         String strTrigger = "RELATED=END:P7W";

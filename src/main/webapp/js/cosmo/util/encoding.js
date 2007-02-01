65ab500faa698a9d43abcdaa09c235a6bd023ffb
travis <travis@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2007-02-01 18:51:31 +0000

    encoding.js module needed by last checkin got overlooked.
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@3548 5fd0b11a-f2f7-0310-948e-c8f80ebed640

---
+++ src/main/webapp/js/cosmo/util/encoding.js
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2007 Open Source Applications Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+
+dojo.provide("cosmo.util.encoding");
+
+cosmo.util.encoding.base64Alphabet = 
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
+    'abcdefghijklmnopqrstuvwxyz' +
+    '0123456789+/='
+    
+cosmo.util.encoding.toBase64 = function(string){
+    var i = 0;
+    var char1, char2, char3;
+    var enc1, enc2, enc3, enc4;
+    
+    var out = '';
+
+    var alpha = cosmo.util.encoding.base64Alphabet;
+    
+    do {
+        char1 = string.charCodeAt(i++);
+        char2 = string.charCodeAt(i++);
+        char3 = string.charCodeAt(i++);
+        
+        enc1  = char1 >> 2;
+        enc2 = ((char1 & 3) << 4) | char2 >> 4;
+        enc3 = ((char2 & 15) << 2) | char3 >> 6;
+        enc4 = char3 & 63;
+        
+        if (isNaN(char2)){
+            enc3 = enc4 = 64;
+        } else if (isNaN(char3)){
+            enc4 = 64;
+        }
+        
+        out = out +
+              alpha.charAt(enc1) +
+              alpha.charAt(enc2) +
+              alpha.charAt(enc3) +
+              alpha.charAt(enc4);
+    } while (i < string.length);
+    
+    return out;
+
+}
+
+cosmo.util.encoding.fromBase64 = function(string){
+    var i = 0;
+    var char1, char2, char3;
+    var enc1, enc2, enc3, enc4;
+    string = string.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+    
+    var out = "";
+
+    var alpha = cosmo.util.encoding.base64Alphabet;
+    
+    do {
+        enc1 = alpha.indexOf(string.charAt(i++));
+        enc2 = alpha.indexOf(string.charAt(i++));
+        enc3 = alpha.indexOf(string.charAt(i++));
+        enc4 = alpha.indexOf(string.charAt(i++));
+        
+        char1 = (enc1 << 2) | (enc2 >> 4);
+        char2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+        char3 = ((enc3 & 3) << 6) | enc4;
+        
+        out = out + String.fromCharCode(char1);
+        
+        if (enc3 != 64) {
+            out = out + String.fromCharCode(char2);
+        }
+        if (enc4 != 64) {
+            out = out + String.fromCharCode(char3);
+        }
+        
+    } while (i < string.length);
+    
+    return out;
+}

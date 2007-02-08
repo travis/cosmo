a2d6da504f2823b00cc9e0605aa810d0bd5b41f6
rletness <rletness@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2007-02-08 17:52:38 +0000

    add commons-id dep
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@3631 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- migration/src/main/resources/0.5.0-to-0.6.0-MySQL5-post.sql
+++ migration/src/main/resources/0.5.0-to-0.6.0-MySQL5-post.sql
@@ -11,4 +11,4 @@ alter table attribute add unique itemid (itemid, namespace, localname);
 alter table cal_property_index add index FKBA988E7927801F2 (eventstampid), add constraint FKBA988E7927801F2 foreign key (eventstampid) references stamp (id) on delete cascade;
 alter table cal_timerange_index add index FK98D277F227801F2 (eventstampid), add constraint FK98D277F227801F2 foreign key (eventstampid) references stamp (id) on delete cascade;
 # update server version
-update server_properties SET propertyvalue='0.6-SNAPSHOT' WHERE propertyname='cosmo.schemaVersion';
+update server_properties SET propertyvalue='0.6.0' WHERE propertyname='cosmo.schemaVersion';

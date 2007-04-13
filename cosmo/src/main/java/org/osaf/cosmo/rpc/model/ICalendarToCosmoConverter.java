cf86184f888616b4029ee871238c4b92512cce5c
br <br@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2007-04-13 23:06:15 +0000

    fix for bug 8782 - for some reason vevent.getStartDate().getTimeZone() doesn't return a timezone even though I can see that the DTSTART object clearly has a TZID. So I just extract the TZID and get the timezone object from the registry.
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@4126 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- cosmo/src/main/java/org/osaf/cosmo/rpc/model/ICalendarToCosmoConverter.java
+++ cosmo/src/main/java/org/osaf/cosmo/rpc/model/ICalendarToCosmoConverter.java
@@ -43,6 +43,8 @@ import net.fortuna.ical4j.model.Parameter;
 import net.fortuna.ical4j.model.Property;
 import net.fortuna.ical4j.model.Recur;
 import net.fortuna.ical4j.model.TimeZone;
+import net.fortuna.ical4j.model.TimeZoneRegistry;
+import net.fortuna.ical4j.model.TimeZoneRegistryFactory;
 import net.fortuna.ical4j.model.component.VEvent;
 import net.fortuna.ical4j.model.component.VTimeZone;
 import net.fortuna.ical4j.model.parameter.Range;
@@ -57,6 +59,7 @@ import net.fortuna.ical4j.model.property.RRule;
 import net.fortuna.ical4j.model.property.RecurrenceId;
 import net.fortuna.ical4j.util.Dates;
 
+import org.apache.commons.lang.StringUtils;
 import org.osaf.cosmo.model.ContentItem;
 import org.osaf.cosmo.model.EventStamp;
 import org.osaf.cosmo.util.ICalendarUtils;
@@ -82,6 +85,9 @@ public class ICalendarToCosmoConverter {
     
     private static Map<EventType, String[]> eventTypesToEventProperties = new HashMap<EventType, String[]>();
     
+    public static TimeZoneRegistry timeZoneRegistry = TimeZoneRegistryFactory
+            .getInstance().createRegistry();
+
     static{
         eventTypesToEventProperties.put(EventType.NORMAL, new String[]{});
         eventTypesToEventProperties.put(EventType.ANYTIME, new String[]{EVENT_ANYTIME, EVENT_START, EVENT_END});
@@ -328,8 +334,9 @@ public class ICalendarToCosmoConverter {
                 Parameter.TZID);
         TimeZone masterTimezone = null;
         if (masterStartDate instanceof DateTime) {
-            DateTime masterStartDateTime = (DateTime) masterStartDate;
-            masterTimezone = masterStartDateTime.getTimeZone();
+            if (StringUtils.isNotBlank(tzid)) {
+                masterTimezone = timeZoneRegistry.getTimeZone(tzid);
+            }
         }
         for (int x = 0; x < startDateList.size(); x++) {
             net.fortuna.ical4j.model.Date instanceStartDate = (net.fortuna.ical4j.model.Date) startDateList

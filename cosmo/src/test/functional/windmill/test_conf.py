f628017d9137fb00cfc0a262c074f5ec7fdd0143
adam <adam@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2007-04-21 01:42:52 +0000

    Fixing tests, removing settings test for wierdness
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@4192 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- cosmo/src/test/functional/windmill/test_conf.py
+++ cosmo/src/test/functional/windmill/test_conf.py
@@ -1,2 +1,1 @@
-test_list = ['create_user_test.json', 'login_with_user.json','settings.json', 'drag_drop_test.json', 'event_properties.json','mini_cal.json','time_zones.json','recurrence.json','admin.json']
-test_list = ['create_user_test.json', 'login_with_user.json','settings.json', 'drag_drop_test.json', 'event_properties.json','mini_cal.json','time_zones.json','recurrence.json','admin.json']
+test_list = ['create_user_test.json', 'login_with_user.json', 'resize_drag_drop.json', 'drag_drop_test.json', 'event_overlap.json','event_properties.json','mini_cal.json','time_zones.json','recurrence.json','admin.json']

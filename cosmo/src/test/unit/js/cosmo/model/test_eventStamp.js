f79f1b3d68eeb2a76f678910ad8de317dd7ce1ab
br <br@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2007-05-02 00:18:42 +0000

    fixes to make tests work with new test structure
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@4298 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- cosmo/src/test/unit/js/cosmo/model/test_eventStamp.js
+++ cosmo/src/test/unit/js/cosmo/model/test_eventStamp.js
@@ -20,58 +20,101 @@ dojo.require("cosmo.model.Item");
 dojo.require("cosmo.model.EventStamp");
 
 cosmotest.model.test_eventStamp = {
-getSetEndDate: function(){
-    var note = cosmotest.model.test_eventStamp.getSimpleEventNote();
-    var stamp = note.getEventStamp();
-    var startDate = new cosmo.datetime.Date(2007,0,1);
-    var endDate = new cosmo.datetime.Date(2007,0,2);
-    stamp.setStartDate(startDate);
-    stamp.setEndDate(endDate);
-    jum.assertTrue(endDate.equals(stamp.getEndDate()));
-    //assert that the duration is one day, in seconds
-    jum.assertEquals(24 * 60 * 60, stamp.getDuration());
+    test_getSetEndDate: function(){
+        var note = cosmotest.model.test_eventStamp.getSimpleEventNote();
+        var stamp = note.getEventStamp();
+        var startDate = new cosmo.datetime.Date(2007,0,1);
+        var endDate = new cosmo.datetime.Date(2007,0,2);
+        stamp.setStartDate(startDate);
+        stamp.setEndDate(endDate);
+        jum.assertTrue(endDate.equals(stamp.getEndDate()));
+        //assert that the duration is one day, in seconds
+        jum.assertEquals(24 * 60 * 60, stamp.getDuration());
+        
+        //let's try that again, now with timezones!
+        //1/1/2007 1pm
+        startDate = new cosmo.datetime.Date(2007,0,1,13,0);
+        startDate.tzId = "America/New_York";
+        endDate = new cosmo.datetime.Date(2007,0,1,14,0);
+        endDate.tzId = "America/New_York";
     
-    //let's try that again, now with timezones!
-    //1/1/2007 1pm
-    startDate = new cosmo.datetime.Date(2007,0,1,13,0);
-    startDate.tzId = "America/New_York";
-    endDate = new cosmo.datetime.Date(2007,0,1,14,0);
-    endDate.tzId = "America/New_York";
+        stamp.setStartDate(startDate);
+        stamp.setEndDate(endDate); 
+        jum.assertTrue(endDate.equals(stamp.getEndDate()));
+        jum.assertEquals(60 * 60, stamp.getDuration()); 
+    },
+    
+    test_occurenceInheritance: function(){
+        var note = cosmotest.model.test_eventStamp.getSimpleEventNote();
+        var startDate = new cosmo.datetime.Date(2007,0,1,13,0);
+        var endDate = new cosmo.datetime.Date(2007,0,1,14,0);
+        var stamp = note.getEventStamp(true);
+        stamp.setStartDate(startDate);
+        stamp.setEndDate(endDate);
+    
+        //Let's pretend that this note occurs weekly, so we'll get the next
+        //occurrence, one that starts one week later.
+        var occurrenceDate = new cosmo.datetime.Date(2007,0,8,13,0);
+        var occurrence = note.getNoteOccurrence(occurrenceDate);
+        var occurStamp = occurrence.getEventStamp();
+        jum.assertTrue(occurrenceDate.equals(occurStamp.getStartDate()));
+    
+        //test overriding start on an occurrence
+        var newOccurrenceStart = new cosmo.datetime.Date(2007,0,8,14,0);
+        occurStamp.setStartDate(newOccurrenceStart);
+        jum.assertTrue(newOccurrenceStart.equals(occurStamp.getStartDate()));
+        
+        //see if the enddate is set properly
+        jum.assertTrue( (new cosmo.datetime.Date(2007,0,8,15,0)).equals(occurStamp.getEndDate()));
+        
+        //make sure the original stamp didn't get messed with
+        jum.assertTrue(stamp.getStartDate().equals(new cosmo.datetime.Date(2007,0,1,13,0)));
+        
+    },
+    
+    //make sure that recurrenceid's get updated properly.
+    test_changingMasterStart: function(){
+        var note = cosmotest.model.test_eventStamp.getSimpleEventNote();
+        var startDate = new cosmo.datetime.Date(2007,0,1,13,0);
+        var endDate = new cosmo.datetime.Date(2007,0,1,14,0);
+        var stamp = note.getEventStamp(true);
+        stamp.setStartDate(startDate);
+        stamp.setEndDate(endDate);
 
-    stamp.setStartDate(startDate);
-    stamp.setEndDate(endDate); 
-    jum.assertTrue(endDate.equals(stamp.getEndDate()));
-    jum.assertEquals(60 * 60, stamp.getDuration()); 
-},
+        //Let's pretend that this note occurs weekly, so we'll get the next
+        //occurrence, one that starts one week later.
+        var occurrenceDate = new cosmo.datetime.Date(2007,0,8,13,0);
+        var occurrence = note.getNoteOccurrence(occurrenceDate);
+        var occurStamp = occurrence.getEventStamp();    
+        occurStamp.setLocation("modlocation");
+        
+        //move the master startdate up one hour!
+        var newStartDate = new cosmo.datetime.Date(2007,0,1,14,0);
+        stamp.setStartDate(newStartDate);
+        
+        //let's make sure the recurrenceId changed! 
+        //there should not be a modification for the old occurrence date
+        var modification = note.getModification(occurrenceDate);
+        jum.assertTrue(modification == null);
+    
+        //but there should be one on the 8th at 14:00
+        var newOccurrenceDate = new cosmo.datetime.Date(2007,0,8,14,0);
+        modification = note.getModification(newOccurrenceDate);
+        jum.assertTrue(modification != null);
 
-test_eventStamp_occurenceInheritance: function(){
-    var note = cosmotest.model.test_eventStamp.getSimpleEventNote();
-    var startDate = new cosmo.datetime.Date(2007,0,1,13,0);
-    var endDate = new cosmo.datetime.Date(2007,0,1,14,0);
-    var stamp = note.getEventStamp(true);
-    stamp.setStartDate(startDate);
-    stamp.setEndDate(endDate);
-
-    //Let's pretend that this note occurs weekly, so we'll get the next
-    //occurrence, one that starts one week later.
-    var occurrenceDate = new cosmo.datetime.Date(2007,0,8,13,0);
-    var occurrence = note.getNoteOccurrence(occurrenceDate);
-    var occurStamp = occurrence.getEventStamp();
-    jum.assertTrue(occurrenceDate.equals(occurStamp.getStartDate()));
-
-    //TODO - test overriding start, end etc.
-},
-
-getSimpleEventNote: function(){
-    var note = new cosmo.model.Note({
-        body: "body",
-        uid: "123",
-        displayName: "display"
-    });
-
-    var eventStamp = note.getEventStamp(true);
-    return note;
-}
+    
+    },
+    
+    getSimpleEventNote: function(){
+        var note = new cosmo.model.Note({
+            body: "body",
+            uid: "123",
+            displayName: "display"
+        });
+    
+        var eventStamp = note.getEventStamp(true);
+        return note;
+    }
 }
 
 

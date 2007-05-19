5438673f86f873a39210e464172026d8faedd974
travis <travis@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2007-05-19 01:51:08 +0000

    Make it possible to run integration tests. This should likely be moved into a different place in the near future.
    Also, to use this one must create a symlink in the webapp directory and set allowLinking="true" in server.xml. More documentation on this forthcoming.
    
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@4449 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- cosmo/src/test/unit/js/cosmo/service/conduits/test_conduits.js
+++ cosmo/src/test/unit/js/cosmo/service/conduits/test_conduits.js
@@ -14,13 +14,46 @@
  * limitations under the License.
  */
 
-dojo.require("cosmoTest.rpc.MockJsonService");
-dojo.require("cosmoTest.service.conduits.*");
-dojo.require("cosmo.service.conduits.jsonrpc");
-function test_conduit(){
-    var s = new cosmoTest.rpc.MockJsonService();
-    s.processSmd(cosmo.rpc.JsonService.defaultSMD);
-    var currentUserConduit = new cosmo.service.conduits.jsonrpc.CurrentUserConduit(s);
-    cosmoTest.service.conduits.testAbstractConduit(currentUserConduit);
-    cosmoTest.service.conduits.testAbstractCurrentUserConduit(currentUserConduit);
-}
+dojo.provide("cosmotest.service.conduits.test_conduits");
+
+dojo.require("cosmo.service.conduits.common");
+
+dojo.debug("Before you run cosmotest.service.translators.test_conduits " +
+        "please make sure you are logged in and have a calendar with " +
+        "at least one Item.");
+cosmotest.service.conduits.test_conduits = {
+    test_Atom: function(){
+        
+        // test getCollections
+        var conduit = cosmo.service.conduits.getAtomPlusEimConduit();
+        var collections = conduit.getCollections({sync: true}).results[0];
+        jum.assertTrue("collections", !!collections);
+        jum.assertTrue("colllections length", collections.length > 0);
+        
+        var c0 = collections[1];
+        
+        // test getCollection
+/*        var collectionDetails = conduit.getCollection(c0.getUid(), {sync: true});
+
+        jum.assertTrue("collectionDetails", !!collectionDetails)
+        dojo.debug(collectionDetails)
+        return */
+        
+        var items = conduit.getItems(c0, {sync: true}).results[0];
+        jum.assertTrue("items", !!items);
+        jum.assertTrue("items length", items.length > 0);
+        
+        var item0 = items[0];
+        item0.setDisplayName("New Display Name");
+        
+        conduit.saveItem(item0, {sync: true});
+        
+        var newItem = new cosmo.model.Note(
+        {
+            displayName: "Testing display name"
+        }
+        );
+        conduit.createItem(newItem, c0, {sync: true});
+    }
+};
+
---
+++ cosmo/src/test/unit/js/setupTester.js
@@ -0,0 +1,111 @@
+  var bu_alert = dojo.hostenv.println;
+  var print = dojo.hostenv.println;
+
+
+  var test_names = jum.alltests_.group_names_;
+  var test_menu = document.getElementById("test-menu");
+  for (var i in test_names) {
+     var row = document.createElement("tr");
+     var col = document.createElement("td");
+     var box = document.createElement("input");
+     box.setAttribute("type","checkbox");
+     box.setAttribute('value',test_names[i]);
+     col.appendChild(box);
+     row.appendChild(col);
+     col = document.createElement("td");
+     col.appendChild(document.createTextNode(test_names[i]));
+     row.appendChild(col);
+     test_menu.appendChild(row);
+   }
+   function checkBoxesFromLocation(){
+       var testNames = location.search.slice(1).split("&");
+   var tests = {};
+   
+   for (var i = 0; i < testNames.length; i++){
+       var testQuery = testNames[i].split("=");
+   if (testQuery[0] == "t"){
+           tests[testQuery[1]] = true;
+       }
+   }
+   var inputs = test_menu.getElementsByTagName("input");
+       for (var i = 0; i < inputs.length; i++){
+           if (tests[inputs[i].value]){
+               inputs[i].checked = true;
+           }
+       }
+   }
+   
+   checkBoxesFromLocation();
+   
+   function getCurrentQueryString(){
+       var inputs = test_menu.getElementsByTagName("input");
+   var qs = "?";
+   var qsElements = 0;
+   for (var i = 0; i < inputs.length; i++){
+       if (inputs[i].checked) {
+              var and = (qsElements > 0)? "&" : "";
+   qs = qs + and + "t=" + inputs[i].value;
+               qsElements++;
+           }
+       }
+       return qs;
+   }
+   
+   
+   function run_tests() {
+       var tests = []
+       var rows = test_menu.getElementsByTagName ("tr");
+   for (var i = 0; i < rows.length; i++) {
+       var test_row = rows[i];
+       if (test_row.nodeType != 1/*Node.ELEMENT_NODE in most browsers*/) continue;
+           if (test_row.childNodes[0].childNodes[0].checked) {
+             jum.runGroup(test_row.childNodes[1].childNodes[0].data);
+           }
+       }
+   }
+   
+   function checkAll(){
+       var inputs = test_menu.getElementsByTagName("input");
+       for (var i = 0; i < inputs.length; i++){
+           inputs[i].checked = true;
+       }
+   }
+   
+   function uncheckAll(){
+       var inputs = test_menu.getElementsByTagName("input");
+       for (var i = 0; i < inputs.length; i++){
+           inputs[i].checked = false;
+       }
+   }
+   
+   function clearOutput(){
+       var out = document.getElementById(djConfig.debugContainerId);
+       out.innerHTML = "";
+   }
+   
+   /*
+   Convenience!
+   */
+   document.onkeyup = function (e){
+       e = !e ? window.event : e;
+   
+   if (String.fromCharCode(e.keyCode) == 'R'){
+       location = location.href.split("?")[0] + getCurrentQueryString();
+   }
+
+   if (String.fromCharCode(e.keyCode) == 'T'){
+        run_tests();
+    }
+
+   if (String.fromCharCode(e.keyCode) == 'U'){
+        uncheckAll();
+    }
+
+   if (String.fromCharCode(e.keyCode) == 'A'){
+        checkAll();
+    }
+
+   if (String.fromCharCode(e.keyCode) == 'C'){
+   clearOutput();
+   }
+ }

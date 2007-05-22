c743c35ef410162ae13e189272d7a56dd8815633
travis <travis@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2007-05-22 20:33:10 +0000

    Add tests for more conduit methods and Event creation
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@4464 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- cosmo/src/test/unit/js/cosmo/service/conduits/test_conduits.js
+++ cosmo/src/test/unit/js/cosmo/service/conduits/test_conduits.js
@@ -17,43 +17,141 @@
 dojo.provide("cosmotest.service.conduits.test_conduits");
 
 dojo.require("cosmo.service.conduits.common");
+dojo.require("cosmo.cmp");
+dojo.require("cosmo.util.auth");
 
-dojo.debug("Before you run cosmotest.service.translators.test_conduits " +
-        "please make sure you are logged in and have a calendar with " +
-        "at least one Item.");
 cosmotest.service.conduits.test_conduits = {
-    test_Atom: function(){
+    test_Note: function(){
+        try{
         
-        // test getCollections
-        var conduit = cosmo.service.conduits.getAtomPlusEimConduit();
-        var collections = conduit.getCollections({sync: true}).results[0];
-        jum.assertTrue("collections", !!collections);
-        jum.assertTrue("colllections length", collections.length > 0);
-        
-        var c0 = collections[1];
-        
-        // test getCollection
-/*        var collectionDetails = conduit.getCollection(c0.getUid(), {sync: true});
+            var user = cosmotest.service.conduits.test_conduits.createTestAccount();
+            
+            // test getCollections
+            var conduit = cosmo.service.conduits.getAtomPlusEimConduit();
+            var collections = conduit.getCollections({sync: true}).results[0];
+            jum.assertTrue("collections", !!collections);
+            jum.assertTrue("collections length", collections.length > 0);
+            
+            var c0 = collections[0];
+            
+            // test getCollection
+            var collectionDetails = conduit.getCollection(c0.getUid(), {sync: true});
+    
+            jum.assertTrue("collectionDetails", !!collectionDetails)
+            
+            
+            // Test createItem
+            var newItemDisplayName = "Testing display name";
+            var newItem = new cosmo.model.Note(
+            {
+                displayName: newItemDisplayName
+            }
+            );
 
-        jum.assertTrue("collectionDetails", !!collectionDetails)
-        dojo.debug(collectionDetails)
-        return */
-        
-        var items = conduit.getItems(c0, {sync: true}).results[0];
-        jum.assertTrue("items", !!items);
-        jum.assertTrue("items length", items.length > 0);
-        
-        var item0 = items[0];
-        item0.setDisplayName("New Display Name");
-        
-        conduit.saveItem(item0, {sync: true});
-        
-        var newItem = new cosmo.model.Note(
-        {
-            displayName: "Testing display name"
+            conduit.createItem(newItem, c0, {sync: true});
+            
+            var item0 = conduit.getItem(newItem.getUid(), {sync: true}).results[0][0];
+            
+            
+            jum.assertEquals("new item display name", newItemDisplayName, item0.getDisplayName());
+            
+            
+            // Test saveItem
+            var item0DisplayName = "New Display Name";
+            item0.setDisplayName(item0DisplayName);
+            
+            conduit.saveItem(item0, {sync: true});
+
+            item0 = conduit.getItem(newItem.getUid(), {sync: true}).results[0][0];
+     
+            jum.assertEquals("item display name", item0DisplayName, item0.getDisplayName());
+
+            // Test getItems
+            conduit.createItem(new cosmo.model.Note(
+            {
+                displayName: "Testing display name 2"
+            }
+            ), c0, {sync: true});
+
+            var items = conduit.getItems(c0, {sync: true}).results[0];
+            jum.assertTrue("items", !!items);
+            jum.assertEquals("items length", 2, items.length);
+            
+            
+            // Test deleteItem
+            conduit.deleteItem(item0.getUid, {sync: true});
+            
+            items = conduit.getItems(c0, {sync: true}).results[0];
+            jum.assertTrue("deleteItem: items", !!items);
+            jum.assertEquals("deleteItem: items length", 1, items.length);
+            
+            
         }
-        );
-        conduit.createItem(newItem, c0, {sync: true});
+        finally{
+            cosmotest.service.conduits.test_conduits.cleanup(user);            
+        }
+    },
+    
+    test_Event: function(){
+        try{
+            var user = cosmotest.service.conduits.test_conduits.createTestAccount();
+            
+            var conduit = cosmo.service.conduits.getAtomPlusEimConduit();
+            var collections = conduit.getCollections({sync: true}).results[0];
+            
+            var c0 = collections[0];
+            
+            var newItem = new cosmo.model.Note(
+            {
+                displayName: "Blah blah blah"
+            }
+            );
+            
+            var startDate = new cosmo.datetime.Date();
+            startDate.setMilliseconds(0);
+            newItem.getEventStamp(true, {
+                startDate: startDate,
+                duration: new cosmo.model.Duration({hour: 1})
+            });
+
+            conduit.createItem(newItem, c0, {sync: true});
+            
+            var item0 = conduit.getItem(newItem.getUid(), {sync: true}).results[0][0];
+            jum.assertTrue("start date", startDate.equals(item0.getEventStamp().getStartDate()));
+            
+            
+        } finally{
+            cosmotest.service.conduits.test_conduits.cleanup(user);            
+        }
+    },
+    
+    createTestAccount: function(){
+       cosmo.util.auth.clearAuth();
+       var user = {
+           password: "testing"
+       };
+       var success = false;
+       
+       var i = 0;
+       while (!success && i < 1000){
+           var un = "user" + i;
+           user.username = un;
+           user.firstName = un;
+           user.lastName = un;
+           user.email = un + "@cosmotesting.osafoundation.org";
+           
+           cosmo.cmp.signup(user, {load: function(){success = true}, error: function(){i++}}, true);
+       }
+       cosmo.util.auth.setCred(user.username, user.password);
+       
+       return user;
+       
+    },
+    
+    cleanup: function(user){
+        cosmo.util.auth.setCred("root", "cosmo");
+        cosmo.cmp.deleteUser(user.username, {handle: function(){}}, true);
+        cosmo.util.auth.clearAuth();
     }
 };
 

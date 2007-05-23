536ae2b2ebf6307740102a5c8bd9ba0b778d6e48
br <br@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2007-05-23 01:23:26 +0000

    testing and implementation of getApplicableChangeTypes() which determines what kind of change application types you can perform to recurring events - that is, "All Events", "Just this Instance" or "This and Future"
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@4476 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- cosmo/src/main/webapp/js/cosmo/model/Delta.js
+++ cosmo/src/main/webapp/js/cosmo/model/Delta.js
@@ -41,6 +41,15 @@ dojo.declare("cosmo.model.Delta", null, {
         return stamp[propertyName]; 
     },
     
+    isPropertyChanged: function(propertyName){
+        return typeof(this.getProperty(propertyName)) != "undefined";
+    },
+    
+    isStampPropertyChanged: function(stampName, propertyName){
+        return typeof(this.getStampProperty(stampName, propertyName)) != "undefined";
+        
+    },
+    
     deltafy: function (){
         // summary: removes all properties which are the same as its note
         // description: removes all properties from the delta which are the same
@@ -49,9 +58,87 @@ dojo.declare("cosmo.model.Delta", null, {
         this._filterOutEqualProperties(this._note, this._propertyProps);
         for (var stampName in this._stampProps){
             var stamp = this._note.getStamp(stampName);
-            var stampChanges = this._stampProps[stamp];
+            if (stamp == null){
+                continue;
+            }
+            var stampChanges = this._stampProps[stampName];
+
+            if (stampChanges == null){
+                continue;
+            }
             this._filterOutEqualProperties(stamp, stampChanges);
         }        
+    },
+    
+    getApplicableChangeTypes: function(){
+        // summary: Returns the types of changes that you can apply with this delta
+        // description: What is returned is a hash which contains "true" for each key
+        //              that represents an applicable change type. Possible valid keys
+        //              are:
+        //                  "master" - you can apply this change to the master event,
+        //                  thereby affecting all occurrences, if it has any.
+        //
+        //                  "occurrence" - you can apply this change to this one 
+        //                  occurrence.
+        //
+        //                  "occurrenceAndFuture" - you can apply this change to this
+        //                  occurrence and all future occurrences.
+        if (!this._note.hasRecurrence()){
+            return {master:true};
+        }
+
+        var rruleChanged = this.isStampPropertyChanged("event", "rrule");
+        //if recurrence rule has changed...
+        
+        if (rruleChanged){
+            //if the rrule's been removed...
+            if (this.getStampProperty("event", "rrule") == null){
+                ///...the only applicable change is to the master event
+                return {master:true};
+            }
+            
+            var oldRule = this._note.getEventStamp().getRrule();
+            var newRule = this.getStampProperty("event", "rrule");
+            
+            //the frequencies are the same, so therefore only the
+            //end date has changed...
+            if (oldRule.getFrequency() == newRule.getFrequency()){
+                return {master:true};
+            }            
+            
+            //the frequency has changed (and maybe end date), but is this
+            //the first occurrence?
+            if (this._note.isMaster() || this._note.isFirstOccurrence()){
+                return {master:true};
+            }
+            
+            //the frequency has changed, and this is NOT the first occurrence
+            return {master:true, occurrenceAndFuture:true};
+        }
+        
+        //if the startdate has moved beyond the recurrence interval...
+        if (this._startDateMovedBeyondRecurrenceFrequency()){
+            //...then changes can only be applied to the occurrence
+            return {occurrence:true};
+        }
+        
+        return { master:true, occurrenceAndFuture:true, occurrence:true};
+    },
+    
+    isApplicableToMaster: function(){
+        return true;        
+    }, 
+    
+    isApplicableToThisAndFuture: function(){
+       if (!note.hasRecurrence()){
+           return false;
+       } 
+    }, 
+    
+    isApplicableToOccurence: function(){
+       if (!note.hasRecurrence()){
+           return false;
+       } 
     },
     
     _filterOutEqualProperties: function (original, changes){
@@ -70,5 +157,33 @@ dojo.declare("cosmo.model.Delta", null, {
             this._stampProps[stampName] = stamp;
         }
         return stamp;        
+    },
+    
+    _startDateMovedBeyondRecurrenceFrequency: function (){
+        // summary: determines if the startDate was moved beyond the range of 
+        // the recurrence interval.
+        // description: If the start date is moved forward one ore more recurrence intervals,
+        // true is returned.
+        // history: Adapted from mde's original code in cosmo.view.cal
+        if (!this.isStampPropertyChanged("event", "startDate")){
+            return false;
+        }
+
+        var changedDate = this.getStampProperty("event", "startDate");
+        var originalDate = this._note.getEventStamp().getStartDate();
+        var frequency = this._note.getEventStamp().getRrule().getFrequency();
+        
+        var unit = this._ranges[frequency][0];
+        var bound = this._ranges[frequency][1];
+        var diff = cosmo.datetime.Date.diff(unit, originalDate, changedDate);
+        return diff >= bound || diff <= (bound * -1)
+    },
+    
+    _ranges: {
+            'daily': [dojo.date.dateParts.DAY, 1],
+            'weekly': [dojo.date.dateParts.WEEK, 1],
+            'biweekly': [dojo.date.dateParts.WEEK, 2],
+            'monthly': [dojo.date.dateParts.MONTH, 1],
+            'yearly': [dojo.date.dateParts.YEAR, 1]
     }
 });
--- cosmo/src/test/unit/js/cosmo/model/test_delta.js
+++ cosmo/src/test/unit/js/cosmo/model/test_delta.js
@@ -26,16 +26,120 @@ dojo.lang.mixin(cosmotest.model.test_delta, {
         var delta = new cosmo.model.Delta(note);
         delta.addProperty("body", "body");
         delta.addProperty("displayName", "new");
-        assertEquals("new", delta.getProperty("displayName"));
-        assertFalse(delta.getProperty("body") == undefined);
+        delta.addStampProperty("fake", "property", "proper");
+        delta.addStampProperty("fake", "nullProperty", null);
+
+        jum.assertEquals("new", delta.getProperty("displayName"));
+        jum.assertFalse(delta.getProperty("body") == undefined);
 
         delta.deltafy();
 
-        assertEquals("new", delta.getProperty("displayName"));
-        assertTrue(delta.getProperty("body") == undefined);
-    }
+        jum.assertEquals("new", delta.getProperty("displayName"));
+        jum.assertTrue(delta.getProperty("body") == undefined);
+        jum.assertFalse(delta.isPropertyChanged("body"));
+        
+        jum.assertTrue("The 'property' property should be changed in 'fake' stamp.",delta.isStampPropertyChanged("fake", "property"));
+        jum.assertTrue(delta.isStampPropertyChanged("fake", "nullProperty"));
+        jum.assertFalse(delta.isStampPropertyChanged("fake", "notAProp"));
+        
+        var stamp = note.getEventStamp(true);
+        stamp.setRrule(new cosmo.model.RecurrenceRule({
+            frequency: cosmo.model.RRULE_FREQUENCIES.FREQUENCY_DAILY
+        }));
+        delta.addStampProperty("event", "rrule",new cosmo.model.RecurrenceRule({
+            frequency: cosmo.model.RRULE_FREQUENCIES.FREQUENCY_DAILY
+        }));
+        delta.deltafy();        
+        jum.assertFalse("RRule should not be changed",delta.isStampPropertyChanged("event", "rrule"));
+        
+    },
+
+    test_getApplicableChangeTypes: function(){
+        function setEquals(set1, set2){
+             var types = ["occurrence", "master", "occurrenceAndFuture"]
+             for (var x = 0; x < types.length; x++ ){
+                 var type = types[x];
+                 if (!!set1[type] != !!set2[type]){
+                     return false;
+                 }
+             }
+             return true;
+        }
+        
+        var note = getSimpleEventNote();
+        var stamp = note.getEventStamp();
+        stamp.setStartDate(new cosmo.datetime.Date(2001,1,1,12,0));
+        var delta = new cosmo.model.Delta(note);
+        delta.addProperty("body","newBody" );
+        delta.deltafy();
+        
+        //first ensure that only master gets returned for non-recurring events
+        jum.assertTrue(setEquals({master:true}, delta.getApplicableChangeTypes()));
+        
+        //now ensure that all types are possible for a simple delta on a recurring item
+        note.getEventStamp().setRrule(new cosmo.model.RecurrenceRule({
+            frequency: cosmo.model.RRULE_FREQUENCIES.FREQUENCY_DAILY
+        }));
+        jum.assertTrue(setEquals({master:true, occurrence:true, occurrenceAndFuture:true}, 
+            delta.getApplicableChangeTypes()));
+        
+        //now the case where we remove the recurrence rule
+        delta.addStampProperty("event", "rrule", null);
+        delta.deltafy();
+        jum.assertTrue("only master allowed when removing the recurrence rule.",setEquals({master:true}, delta.getApplicableChangeTypes()));
+        
+        //now the case where we modify the recurrence rule, but only the endDate (not frequency)
+        delta.addStampProperty("event", "rrule", new cosmo.model.RecurrenceRule({
+            frequency: cosmo.model.RRULE_FREQUENCIES.FREQUENCY_DAILY,
+            endDate: new cosmo.datetime.Date(2000,1,1)}));
+        delta.deltafy();
+        jum.assertTrue("only master allowed when recurrence end date is modified, but not the frequency", 
+            setEquals({master:true}, delta.getApplicableChangeTypes()));
+        
+        //the frequency has changed, and this is the master event
+        delta.addStampProperty("event", "rrule", new cosmo.model.RecurrenceRule({
+            frequency: cosmo.model.RRULE_FREQUENCIES.FREQUENCY_WEEKLY
+        }));
+        delta.deltafy();
+        jum.assertTrue("only master allowed when freq.is modified, and is master", 
+            setEquals({master:true}, delta.getApplicableChangeTypes()));
+
+        //the frequency has changed, and this is the first occurrence
+        var occurrence = note.getNoteOccurrence(new cosmo.datetime.Date(2001,1,1,12,0));
+        delta = new cosmo.model.Delta(occurrence);
+        delta.addStampProperty("event", "rrule", new cosmo.model.RecurrenceRule({
+            frequency: cosmo.model.RRULE_FREQUENCIES.FREQUENCY_WEEKLY
+        }));
+        jum.assertTrue("only master allowed when freq.is modified, and is first occurrence", 
+            setEquals({master:true}, delta.getApplicableChangeTypes()));
+        
+        //the frequency has changed, and this is NOT first occurrence
+        var occurrence = note.getNoteOccurrence(new cosmo.datetime.Date(2001,1,2,12,0));
+        delta = new cosmo.model.Delta(occurrence);
+        delta.addStampProperty("event", "rrule", new cosmo.model.RecurrenceRule({
+            frequency: cosmo.model.RRULE_FREQUENCIES.FREQUENCY_WEEKLY
+        }));
+        jum.assertTrue("'master' and 'occurrenceAndFuture' are allowed when rrule frequency has changed and it is NOT the first occurrence", 
+            setEquals({master:true, occurrenceAndFuture:true}, delta.getApplicableChangeTypes()));
+        
+        //the start date has moved beyond the recurrence interval
+        var occurrence = note.getNoteOccurrence(new cosmo.datetime.Date(2001,1,2,12,0));
+        delta = new cosmo.model.Delta(occurrence);
+        delta.addStampProperty("event", "startDate", new cosmo.datetime.Date(2001,1,4,12,0));
+        jum.assertTrue("only 'occurrence' allowed when you move the start date beyond the recurrence interval range", 
+            setEquals({occurrence:true}, delta.getApplicableChangeTypes()));
+        
+        //sanity check: move the start date just a little, well within recurrence interval
+        var occurrence = note.getNoteOccurrence(new cosmo.datetime.Date(2001,1,2,12,0));
+        delta = new cosmo.model.Delta(occurrence);
+        delta.addStampProperty("event", "startDate", new cosmo.datetime.Date(2001,1,2,13,0));
+        jum.assertTrue("just moving the start date up a tad, ALL change types should be allowed.", 
+            setEquals({ master:true, occurrenceAndFuture:true, occurrence:true}, delta.getApplicableChangeTypes()));
+   }
+ 
 });
 
+    
 function getSimpleEventNote(){
     var note = new cosmo.model.Note({
         body: "body",

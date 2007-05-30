db404814f5eb5da5a05065821c3d292b5d059ce5
travis <travis@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2007-05-30 01:56:33 +0000

    Cleanup whitespace and make sure nls directory is included in build. Not including this was causing problems in the built version of our js libraries.
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@4538 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- dojo/cosmo.profile.js
+++ dojo/cosmo.profile.js
@@ -1,12 +1,13 @@
 var dependencies = [
-        "cosmo.app",
-	"cosmo.ui.cal_main",
-	"cosmo.ui.global_css",
-	"cosmo.ui.event.listeners",
-	"dojo.debug.console",
-	"dojo.logging.ConsoleLogger",
-	"dojo.logging.Logger",
-	"cosmo.ui.widget.Debug",
+    "cosmo.app",
+    "cosmo.app.pim", 
+    "cosmo.ui.cal_main",
+    "cosmo.ui.global_css",
+    "cosmo.ui.event.listeners",
+    "dojo.debug.console",
+    "dojo.logging.ConsoleLogger",
+    "dojo.logging.Logger",
+    "cosmo.ui.widget.Debug",
     "cosmo.datetime.timezone.LazyCachingTimezoneRegistry"
 ];
 

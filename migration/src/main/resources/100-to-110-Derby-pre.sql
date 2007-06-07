5722912d7c871ebcaf8778424f525536a51cd36b
rletness <rletness@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2007-06-07 18:22:49 +0000

    migrate user_preferences table
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@4610 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- migration/src/main/resources/100-to-110-Derby-pre.sql
+++ migration/src/main/resources/100-to-110-Derby-pre.sql
@@ -12,5 +12,13 @@ alter table event_stamp add column enddate varchar(16)
 create index idx_startdt on event_stamp (startdate)
 create index idx_enddt on event_stamp (enddate)
 
+# user_preferences
+rename table user_preferences to x_user_preferences
+create table user_preferences (id bigint not null, createdate bigint, modifydate bigint, preferencename varchar(255) not null, preferencevalue varchar(255) not null, userid bigint not null, primary key (id), unique (userid, preferencename))
+
+
 # migrate data
 update item set itemtype='file' where itemtype='content'
+insert into user_preferences (userid, preferencename, preferencevalue, createdate, modifydate) select userid, preferencename, preferencevalue, 1181239766000, 1181239766000 from x_user_preferences
+drop table x_user_preferences
+alter table user_preferences add constraint FK199BD08467D36616 foreign key (userid) references users
--- migration/src/main/resources/100-to-110-MySQL5-pre.sql
+++ migration/src/main/resources/100-to-110-MySQL5-pre.sql
@@ -12,5 +12,12 @@ alter table event_stamp add column enddate varchar(16);
 alter table event_stamp add index idx_startdt (startdate);
 alter table event_stamp add index idx_enddt (enddate);
 
+# user_preferences
+alter table user_preferences rename to x_user_preferences;
+create table user_preferences (id bigint not null auto_increment, createdate bigint, modifydate bigint, preferencename varchar(255) not null, preferencevalue varchar(255) not null, userid bigint not null, primary key (id), unique (userid, preferencename)) ENGINE=InnoDB;
+
 # migrate data
 update item set itemtype='file' where itemtype='content';
+insert into user_preferences (userid, preferencename, preferencevalue, createdate, modifydate) select userid, preferencename, preferencevalue, UNIX_TIMESTAMP()*1000, UNIX_TIMESTAMP()*1000 from x_user_preferences;
+drop table x_user_preferences;
+alter table user_preferences add index FK199BD08467D36616 (userid), add constraint FK199BD08467D36616 foreign key (userid) references users (id);

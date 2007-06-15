14a7cb6a3b801d88f96d5885101fc4a6b7252e6c
travis <travis@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2007-06-15 00:53:15 +0000

    test for uri generation code.
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@4697 5fd0b11a-f2f7-0310-948e-c8f80ebed640

---
+++ cosmo/src/test/unit/js/cosmo/service/transport/test_Atom.js
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2007 Open Source Applications Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+dojo.provide("cosmotest.service.transport.test_Atom");
+
+dojo.require("cosmo.service.transport.Atom");
+
+//Initialization.
+//TODO - once Dojo implements setUp() and tearDown() move this code there.
+
+cosmotest.service.transport.test_Atom = {
+    test_generateUri: function(){
+        var at = new cosmo.service.transport.Atom();
+        var u = at.generateUri("a/b", "/c", {d:"e"})
+        jum.assertEquals("no query base uri generate unsuccessful", "a/b/c?d=e", u)
+
+        u = at.generateUri("a/b?g=h", "/c", {d:"e"})
+        jum.assertEquals("query base uri generate unsuccessful", "a/b/c?d=e&g=h", u)
+        
+    }  
+}

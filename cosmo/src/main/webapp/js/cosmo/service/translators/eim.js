76da134e0b201461d9d3df67614b8b9179acd703
travis <travis@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2007-06-18 21:48:41 +0000

    Fix client for r4714. Also, noticed a bug code that sends empty note records on modifications.
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@4717 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- cosmo/src/main/webapp/js/cosmo/service/translators/eim.js
+++ cosmo/src/main/webapp/js/cosmo/service/translators/eim.js
@@ -529,7 +529,7 @@ dojo.declare("cosmo.service.translators.Eim", null, {
 
         if (this.modificationHasNoteModifications(modification))
             records.note = this.modifiedOccurrenceToNoteRecord(noteOccurrence);
-        else records.note = this.generateEmptyItem(noteOccurrence);
+        else records.note = this.generateEmptyNote(noteOccurrence);
         
         records.event = this.modifiedOccurrenceToEventRecord(noteOccurrence)
         
@@ -762,8 +762,8 @@ dojo.declare("cosmo.service.translators.Eim", null, {
             if (props.status != undefined) fields.status = [type.TEXT, props.status];
             if (props.location != undefined) fields.location = [type.TEXT, props.location];
             if (props.duration != undefined) fields.duration = [type.TEXT, props.duration.toIso8601()];
-            if (props.rrule != undefined) fields.rrule = [type.TEXT, this.rruleToICal(props.rrule)];
-            if (props.exdates != undefined) fields.exdates = [type.TEXT, this.exdatesToEim(props.exdates)];
+            if (props.rrule != undefined) fields.rrule = [type.CLOB, this.rruleToICal(props.rrule)];
+            if (props.exdates != undefined) fields.exdates = [type.CLOB, this.exdatesToEim(props.exdates)];
             
             return record = {
                 prefix: prefix.EVENT,

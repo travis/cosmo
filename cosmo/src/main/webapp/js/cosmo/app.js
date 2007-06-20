b63bc115055373fcafa32b2e9a259edafbac9a83
travis <travis@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2007-06-20 18:57:40 +0000

    Make sure we require dojo.io, since we're using it.
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@4743 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- cosmo/src/main/webapp/js/cosmo/app.js
+++ cosmo/src/main/webapp/js/cosmo/app.js
@@ -17,6 +17,7 @@
 dojo.provide('cosmo.app');
 
 dojo.require("dojo.event.*");
+dojo.require("dojo.io.*");
 dojo.require('cosmo.ui.widget.ModalDialog');
 dojo.require("cosmo.ui.button");
 dojo.require("cosmo.util.i18n");

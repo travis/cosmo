e736e576c5bc4d417e6dbe49d68fb07381f191a3
rletness <rletness@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2007-07-02 19:59:21 +0000

    add BufferedRequestWrapper, that allows requests to be buffered
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@4919 5fd0b11a-f2f7-0310-948e-c8f80ebed640

---
+++ cosmo/src/main/java/org/osaf/cosmo/filters/BufferedRequestWrapper.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2007 Open Source Applications Foundation
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.osaf.cosmo.filters;
+
+import java.io.IOException;
+
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+import org.osaf.cosmo.dav.impl.BufferedServletInputStream;
+
+/**
+ * HttpServletRequestWrapper that allows a BufferedServletInputStream to be
+ * used as part of a given HttpServletRequest.  This delegates all methods
+ * to a given HttpServletRequest except for "getInputStream", which
+ * returns the instance of BufferedServletInputStream.
+ */
+public class BufferedRequestWrapper extends HttpServletRequestWrapper {
+    
+    private BufferedServletInputStream inputStream = null;
+    private boolean retryRequest = false;
+    
+    public BufferedRequestWrapper(HttpServletRequest request) throws IOException {
+        super(request);
+        inputStream = new BufferedServletInputStream(request.getInputStream());
+    }
+
+    /**
+     * When called, next call to getInputStream() will reset the buffered
+     * inputstream, allowing the request to be re-processed
+     */
+    public void retryRequest() {
+        retryRequest = true;
+    }
+    
+    @Override
+    public ServletInputStream getInputStream() throws IOException {
+        if(retryRequest==true) {
+            retryRequest = false;
+            inputStream.resetToBeginning();
+        }
+        return inputStream;
+    }
+}

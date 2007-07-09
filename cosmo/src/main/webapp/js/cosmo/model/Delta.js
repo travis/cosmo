25c6df6cdc8a985f9622cf49052716cd83d35e77
br <br@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2007-07-09 23:31:45 +0000

    removed irritating debug message
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@4999 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- cosmo/src/main/webapp/js/cosmo/model/Delta.js
+++ cosmo/src/main/webapp/js/cosmo/model/Delta.js
@@ -370,7 +370,6 @@ dojo.declare("cosmo.model.Delta", null, {
         
 //        dojo.debug("delta._apply(): applying stamp changes");
         for (var stampName in this._stampProps){
-            dojo.debug("delta._apply(): applying stamp changes for stamp: " +stampName);
             var stampChanges = this._stampProps[stampName];
             if (stampChanges == null || this._isEmpty(stampChanges)){
                 dojo.debug("delta.apply(): no actual changes for stamp '" + stampName +"'");

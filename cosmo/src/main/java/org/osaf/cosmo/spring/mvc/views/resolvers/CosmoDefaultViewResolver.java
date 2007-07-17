afc21ecc749221f2225200a29b5214975d48df33
travis <travis@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2007-07-17 23:30:25 +0000

    Bug 9694: Make sure non-existent resources under paths handled by spring mvc return 404s (with pretty response pages)
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@5115 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- cosmo/src/main/java/org/osaf/cosmo/spring/mvc/views/resolvers/CosmoDefaultViewResolver.java
+++ cosmo/src/main/java/org/osaf/cosmo/spring/mvc/views/resolvers/CosmoDefaultViewResolver.java
@@ -1,19 +1,28 @@
-package org.osaf.cosmo.spring.mvc.views.resolvers;
+/*
+ * Copyright 2007 Open Source Applications Foundation
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */package org.osaf.cosmo.spring.mvc.views.resolvers;
 
 import java.util.Locale;
 
 import org.springframework.web.servlet.View;
-import org.springframework.web.servlet.view.InternalResourceView;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
-
+import org.springframework.web.servlet.view.ResourceBundleViewResolver;
 
 public class CosmoDefaultViewResolver extends InternalResourceViewResolver {
-    private String requestContextAttribute = "";
-    
-    public void setRequestContextAttribute(String requestContextAttribute) {
-        this.requestContextAttribute = requestContextAttribute;
-    }
+    private ResourceBundleViewResolver notFoundViewResolver;
+    private String notFoundViewName;
     
     @Override
     protected View loadView(String viewName, Locale locale) throws Exception{
@@ -21,11 +30,21 @@ public class CosmoDefaultViewResolver extends InternalResourceViewResolver {
         if (viewName.startsWith("forward:") || viewName.startsWith("redirect:")){
             return super.loadView(viewName, locale);
         } else {
-            InternalResourceView view = new InternalResourceView();
-            view.setUrl("/WEB-INF/jsp/error/notfound.jsp");
-            view.setRequestContextAttribute(this.requestContextAttribute);
-            return view;
+            return notFoundViewResolver.resolveViewName(notFoundViewName, locale);
         }
     
+    }
+
+    public void setNotFoundViewResolver(
+            ResourceBundleViewResolver notFoundViewResolver) {
+        this.notFoundViewResolver = notFoundViewResolver;
+    }
+
+    public String getNotFoundViewName() {
+        return notFoundViewName;
+    }
+
+    public void setNotFoundViewName(String notFoundViewName) {
+        this.notFoundViewName = notFoundViewName;
     }
 }

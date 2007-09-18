823758ad46cc4a3076d40f7dd8b2246c49420f85
mde <mde@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2007-09-18 23:25:30 +0000

    Porting r5684 and r5693 from 0.7.3 branch to trunk (native JS tests for Windmill).
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@5734 5fd0b11a-f2f7-0310-948e-c8f80ebed640

---
+++ cosmo/src/test/functional/windmill/initialize.js
@@ -0,0 +1,12 @@
+// Some environment setup
+var app = windmill.testingApp;
+var cosmo = app.cosmo;
+
+// The tests, in order
+windmill.jsTest.registerTests([
+  'fixture_unsavedSetUpLozenges',
+  'test_unsavedClickLozengeDiscard',
+  'test_unsavedClickLozengeSave',
+  'test_unsavedViewChangeDiscard',
+  'test_unsavedViewChangeSave'
+]);
---
+++ cosmo/src/test/functional/windmill/unsaved_changes.js
@@ -0,0 +1,87 @@
+
+var fixture_unsavedSetUpLozenges = [
+  { method: "doubleClick", params: { id: "hourDiv0-1200" } },
+  { method: "waits.sleep", params: { milliseconds: 3000 } },
+  { method: "doubleClick", params: { id: "hourDiv1-1200" } },
+  { method: "waits.sleep", params: { milliseconds: 3000 } }
+];
+
+var test_unsavedClickLozengeDiscard = [
+  { method: 'extensions.clickLozenge', params: { jsid: "windmill.testingApp.cosmo.view.cal.itemRegistry.getAtPos(1).id" } },
+  { method: "type", params: { id: "noteTitle", text: "Unsaved Lozenge" } },
+  { method: 'extensions.clickLozenge', params: { jsid: "windmill.testingApp.cosmo.view.cal.itemRegistry.getAtPos(0).id" } },
+  { method: "waits.sleep", params: { milliseconds: 4000 } },
+  function () {
+    var dialogText = app.$('modalDialogContent').innerHTML;
+    jum.assertEquals(dialogText, "You have unsaved changes in the item-detail form. What do you want to do?");
+  },
+  { method: "click", params: { jsid: "cosmo.app.modalDialog.btnsRight[0].domNode.id" } },
+  { method: "waits.sleep", params: { milliseconds: 4000 } },
+  function () {
+    var summaryText = app.$('noteTitle').value;
+    jum.assertEquals(summaryText, 'New Event');
+  }
+];
+
+var test_unsavedClickLozengeSave = [
+  { method: "waits.sleep", params: { milliseconds: 4000 } },
+  { method: 'extensions.clickLozenge', params: { jsid: "windmill.testingApp.cosmo.view.cal.itemRegistry.getAtPos(1).id" } },
+  { method: "type", params: { id: "noteTitle", text: "Unsaved Lozenge" } },
+  { method: 'extensions.clickLozenge', params: { jsid: "windmill.testingApp.cosmo.view.cal.itemRegistry.getAtPos(0).id" } },
+  { method: "waits.sleep", params: { milliseconds: 4000 } },
+  function () {
+    var dialogText = app.$('modalDialogContent').innerHTML;
+    jum.assertEquals(dialogText, "You have unsaved changes in the item-detail form. What do you want to do?");
+  },
+  { method: "click", params: { jsid: "cosmo.app.modalDialog.btnsRight[1].domNode.id" } },
+  { method: "waits.sleep", params: { milliseconds: 8000 } },
+  function () {
+    var summaryText = app.$('noteTitle').value;
+    jum.assertEquals(summaryText, 'Unsaved Lozenge');
+  }
+];
+
+var test_unsavedViewChangeDiscard = [
+  { method: "waits.sleep", params: { milliseconds: 4000 } },
+  { method: 'extensions.clickLozenge', params: { jsid: "windmill.testingApp.cosmo.view.cal.itemRegistry.getAtPos(1).id" } },
+  { method: "type", params: { id: "noteTitle", text: "Unsaved View Change" } },
+  { method: "click", params: { jsid: "cosmo.app.pim.layout.baseLayout.mainApp.centerColumn.navBar.viewToggle.buttonNodes[0].id" } },
+  { method: "waits.sleep", params: { milliseconds: 4000 } },
+  function () {
+    var dialogText = app.$('modalDialogContent').innerHTML;
+    jum.assertEquals(dialogText, "You have unsaved changes in the item-detail form. What do you want to do?");
+  },
+  { method: "click", params: { jsid: "cosmo.app.modalDialog.btnsRight[0].domNode.id" } },
+  { method: "waits.sleep", params: { milliseconds: 8000 } },
+  function () {
+    var titleCol = app.$('listViewTitleHeader');
+    jum.assertEquals(titleCol.innerHTML, 'Title');
+  }
+];
+
+var test_unsavedViewChangeSave = [
+  { method: "waits.sleep", params: { milliseconds: 4000 } },
+  { method: "click", params: { jsid: "cosmo.app.pim.layout.baseLayout.mainApp.centerColumn.navBar.viewToggle.buttonNodes[1].id" } },
+  { method: 'extensions.clickLozenge', params: { jsid: "windmill.testingApp.cosmo.view.cal.itemRegistry.getAtPos(1).id" } },
+  { method: "type", params: { id: "noteTitle", text: "Unsaved View Change" } },
+  { method: "click", params: { jsid: "cosmo.app.pim.layout.baseLayout.mainApp.centerColumn.navBar.viewToggle.buttonNodes[0].id" } },
+  { method: "waits.sleep", params: { milliseconds: 4000 } },
+  function () {
+    var dialogText = app.$('modalDialogContent').innerHTML;
+    jum.assertEquals(dialogText, "You have unsaved changes in the item-detail form. What do you want to do?");
+  },
+  { method: "click", params: { jsid: "cosmo.app.modalDialog.btnsRight[1].domNode.id" } },
+  { method: "waits.sleep", params: { milliseconds: 8000 } },
+  function () {
+    var titleCol = app.$('listViewTitleHeader');
+    jum.assertEquals(titleCol.innerHTML, 'Title');
+  },
+  { method: "click", params: { jsid: "cosmo.app.pim.layout.baseLayout.mainApp.centerColumn.navBar.viewToggle.buttonNodes[1].id" } },
+  { method: "waits.sleep", params: { milliseconds: 8000 } },
+  function () {
+    var summaryText = app.$('noteTitle').value;
+    jum.assertEquals(summaryText, 'Unsaved View Change');
+  }
+];
+
+

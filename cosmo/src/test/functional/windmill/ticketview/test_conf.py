608ad59c2709f32eb04edd296282ac589a1f846c
adam <adam@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2007-09-19 18:01:31 +0000

    Fixing ticketview again
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@5740 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- cosmo/src/test/functional/windmill/ticketview/test_conf.py
+++ cosmo/src/test/functional/windmill/ticketview/test_conf.py
@@ -1,1 +1,1 @@
-test_list = ['ticketview_login.json', 'ticketview_settings.json','ticketview_ab.json','ticketview_load.json','ticketview_test.json']
+test_list = [,'ticketview_login.json', 'ticketview_settings.json','ticketview_ab.json','ticketview_load.json','ticketview_test.json']

d619c4ff547ee22741eac16d8e5e5d960bfb603b
travis <travis@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2007-10-18 17:56:29 +0000

    Bug 11074: Patch 0.4.3 build system to remove dojo.requires that use single quotes.
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@5980 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- dojo/build.sh
+++ dojo/build.sh
@@ -3,6 +3,7 @@ DOJO_VERSION="release-0.4.3"
 if [ ! -d "$DOJO_VERSION" ]; then
     svn co http://svn.dojotoolkit.org/dojo/tags/$DOJO_VERSION
     cd $DOJO_VERSION/buildscripts
+    patch -p0 < ../../0.4.3-buildUtil.js.patch
     ant fix-config
 fi
 cd $DOJO_VERSION/buildscripts

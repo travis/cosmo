7fae9f18e2a125494fe0858fa047535380ca13dd
rletness <rletness@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2007-10-23 17:21:51 +0000

    bug 11105 workaround for weird hibernate error Remove deprecated supportedComponentSet attribute as that is what seems to be causes this.
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@6037 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- migration/src/main/resources/120-to-130-Derby-pre.sql
+++ migration/src/main/resources/120-to-130-Derby-pre.sql
@@ -2,3 +2,7 @@
 
 # migrate data
 update event_stamp set isfloating=1 where startdate not like '%T%'
+
+# remove old data
+delete from multistring_values where exists (select id from attribute where id=attributeid and namespace='org.osaf.cosmo.model.CalendarCollectionStamp' and localname='supportedComponentSet')
+delete from attribute where namespace='org.osaf.cosmo.model.CalendarCollectionStamp' and localname='supportedComponentSet'

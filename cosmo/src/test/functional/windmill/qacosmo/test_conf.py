5ef5518206e2307cff0013765a56d7fecdb01391
adam <adam@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2007-10-23 01:31:30 +0000

    Replicating my qacosmo updates to the windmill tests in the trunk
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@6035 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- cosmo/src/test/functional/windmill/qacosmo/test_conf.py
+++ cosmo/src/test/functional/windmill/qacosmo/test_conf.py
@@ -1,1 +1,35 @@
-test_list = ['initialize.json','create_user_test.json', 'login_with_user.json','mini_cal.json','recurrence_dialog_test.json','dash_address_item.json','dash_allday_item.json','dash_multiple_notes.json','dash_now_later_done.json','dash_quick_create.json', 'resize_drag_drop.json', 'drag_drop_test.json', 'event_overlap.json','event_properties.json','time_zones.json','recurrence.json','ticketview/ticketview_login.json', 'ticketview/ticketview_settings.json','ticketview/ticketview_ab.json','ticketview/ticketview_load.json','ticketview/ticketview_test.json']
+test_list = [
+
+#setup tests
+'../setup/initialize.json',
+'setup/create_user_test.json',
+'../setup/login_with_user.json',
+
+#mini-cal tests
+'../minical/mini_cal.json',
+
+#canvas recurrence test
+'../canvas/recurrence/recurrence_dialog_test.json',
+'../canvas/recurrence/recurrence.json',
+
+#drag and drop and resize tests
+'../canvas/dnd/resize_drag_drop.json', 
+'../canvas/dnd/drag_drop_test.json',
+
+#event tests
+'../canvas/event/event_overlap.json',
+'../canvas/event/event_properties.json',
+
+#timezone tests
+'../canvas/tz/time_zones.json',
+
+#dashboard tests
+'../dashboard/dash_address_item.json',
+'../dashboard/dash_allday_item.json',
+'../dashboard/dash_multiple_notes.json',
+'../dashboard/dash_now_later_done.json',
+'../dashboard/dash_quick_create.json',
+
+#tear down
+'../teardown/logout.json'
+]
--- cosmo/src/test/functional/windmill/qacosmo/ticketview/test_conf.py
+++
@@ -1,1 +0,0 @@
-test_list = ['ticketview_login.json', 'ticketview_settings.json','ticketview_ab.json','ticketview_load.json','ticketview_test.json']

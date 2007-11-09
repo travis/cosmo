e7a2bf4402a515a27763c81495dcbe6b29c85017
adam <adam@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2007-11-09 09:19:18 +0000

    Automated tests for dashboard paging, collection adding and removing
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@6156 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- cosmo/src/test/functional/windmill/test_conf.py
+++ cosmo/src/test/functional/windmill/test_conf.py
@@ -8,6 +8,9 @@ test_list = [
 #Event, canvas refresh test
 'canvas/event/refresh.json',
 
+#collections tests
+'collections/collection_add_remove.json',
+
 #mini-cal tests
 'minical/mini_cal.json',
 
@@ -35,6 +38,7 @@ test_list = [
 'dashboard/dash_multiple_notes.json',
 'dashboard/dash_now_later_done.json',
 'dashboard/dash_quick_create.json',
+'dashboard/dash_paging.json',
 
 #create collection
 'create_collection.json',

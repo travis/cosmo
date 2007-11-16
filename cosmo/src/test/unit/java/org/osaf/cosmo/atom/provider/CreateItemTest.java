d0ae2f27b5fe51f07855d1c6ecf2b1a93ccd8bb3
bcm <bcm@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2007-11-16 18:45:09 +0000

    Content-Location is no longer set for create media requests
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@6211 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- cosmo/src/test/unit/java/org/osaf/cosmo/atom/provider/CreateItemTest.java
+++ cosmo/src/test/unit/java/org/osaf/cosmo/atom/provider/CreateItemTest.java
@@ -48,7 +48,6 @@ public class CreateItemTest extends BaseItemProviderTestCase
         assertNotNull("Null etag", res.getEntityTag());
         assertNotNull("Null last modified", res.getLastModified());
         assertNotNull("Null Location header", res.getLocation());
-        assertNotNull("Null Content-Location header", res.getContentLocation());
     }
 
     public void testCreateMediaEvent() throws Exception {
@@ -68,7 +67,6 @@ public class CreateItemTest extends BaseItemProviderTestCase
         assertNotNull("Null etag", res.getEntityTag());
         assertNotNull("Null last modified", res.getLastModified());
         assertNotNull("Null Location header", res.getLocation());
-        assertNotNull("Null Content-Location header", res.getContentLocation());
     }
 
     public void testUnsupportedContentType() throws Exception {

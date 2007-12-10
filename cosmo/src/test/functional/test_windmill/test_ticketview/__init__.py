58513e1c30eb16799efdbdc3466bed694365ee97
adam <adam@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2007-12-10 08:15:13 +0000

    Checking in a working ticketview test
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@6367 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- cosmo/src/test/functional/test_windmill/test_ticketview/__init__.py
+++ cosmo/src/test/functional/test_windmill/test_ticketview/__init__.py
@@ -1,4 +1,25 @@
-ticket_view_login = """
+ticket_view_lab_login = """
+{"method": "waits.forElement", "params": {"link": "Sign up."}}
+{"params": {"link": "Sign up."},  "method": "click"}
+{"method": "waits.forElement", "params": {"id": "modalDialogTitle", "timeout": 40000}}
+{"params": {"id": "tos"},  "method": "check"}
+{"params": {"text": "{$random1}", "id": "username"},  "method": "type"}
+{"params": {"text": "{$random1}", "id": "firstName"},  "method": "type"}
+{"params": {"text": "{$random1}", "id": "lastName"},  "method": "type"}
+{"params": {"text": "{$random1}@osafoundation.org", "id": "email"},  "method": "type"}
+{"params": {"text": "{$random1}", "id": "password"},  "method": "type"}
+{"params": {"text": "{$random1}", "id": "confirm"},  "method": "type"}
+{"params": {"jsid": "{$btnsRight0}"},  "method": "click"}
+{"method": "waits.forElement", "params": {"id": "modalDialogPrompt", "timeout": 40000}}
+{"method": "waits.sleep", "params": {"milliseconds" : 1000}}
+{"method": "click", "params": {"jsid" : "{$btnsCenter0}"}}
+{"params": {"id": "loginDialogUsernameInput"},  "method": "click"}
+{"params": {"text": "{$random1}", "id": "loginDialogUsernameInput"},  "method": "type"}
+{"params": {"text": "{$random1}", "id": "loginDialogPasswordInput"},  "method": "type"}
+{"method": "click", "params": {"id" : "loginSubmitButton"}}
+"""
+
+ticket_view_snarf_login = """
 {"method": "waits.forElement", "params": {"link": "Sign up."}}
 {"params": {"link": "Sign up."},  "method": "click"}
 {"method": "waits.forElement", "params": {"id": "modalDialogTitle", "timeout": 40000}}
@@ -10,6 +31,7 @@ ticket_view_login = """
 {"params": {"text": "{$random1}", "id": "confirm"},  "method": "type"}
 {"params": {"jsid": "{$btnsRight0}"},  "method": "click"}
 {"method": "waits.forElement", "params": {"id": "modalDialogPrompt", "timeout": 40000}}
+{"method": "waits.sleep", "params": {"milliseconds" : 1000}}
 {"method": "click", "params": {"jsid" : "{$btnsCenter0}"}}
 {"params": {"id": "loginDialogUsernameInput"},  "method": "click"}
 {"params": {"text": "{$random1}", "id": "loginDialogUsernameInput"},  "method": "type"}
@@ -27,7 +49,17 @@ ticket_view_settings = """
 {"params": {"milliseconds": 3000},  "method": "waits.sleep"}
 {"params": {"jsid": "{$btnsRight0}"},  "method": "click"}"""
 
+from windmill.authoring import enable_collector, RunJsonFile
+import windmill
+lab_urls = ['http://lab.osaf.us', 'http://next.osaf.us']
+
 def setup_module(module):
-    RunJsonFile('ticket_view_login.json', lines=ticket_view_login.splitlines())()
+    if windmill.settings['TEST_URL'] in lab_urls:
+        json = ticket_view_lab_login
+    else:
+        json =  ticket_view_snarf_login
+    RunJsonFile('create_user.json', lines=json.splitlines())()
     RunJsonFile('ticket_view_settings.json', lines=ticket_view_settings.splitlines())()
 
+def teardown_module(module):
+    RunJsonFile('log_out.json', lines=['{"method": "click", "params": {"link" : "Log in"}}'])()

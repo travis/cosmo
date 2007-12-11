88ccc01fc14bb854e7eb9021e0059b9cf7322714
travis <travis@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2007-12-11 22:04:29 +0000

    More work to get admin js tests running. Currently commented out in register.js.
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@6377 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- cosmo/src/test/functional/test_windmill/js/register.js
+++ cosmo/src/test/functional/test_windmill/js/register.js
@@ -1,4 +1,5 @@
-
+windmill.jsTest.registerTestNamespace('webuiInit');
 windmill.jsTest.registerTestNamespace('unsavedChangesTests');
 windmill.jsTest.registerTestNamespace('opacityTests');
-windmill.jsTest.registerTestNamespace('adminTests');
+//windmill.jsTest.registerTestNamespace('adminTests');
+

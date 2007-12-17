8ca533ad41368016ac4e2bcbb96477586676ab6f
br <br@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2007-12-17 22:54:53 +0000

    Update dojo build to 1.0.2
    --note - Travis did the thinking. I mostly just typed. 
    
    
    
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/branches/dojo1_0@6416 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- dojo/build.sh
+++ dojo/build.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
-DOJO_VERSION="release-0.4.3"
+DOJO_VERSION="release-1.0.2"
 if [ ! -d "$DOJO_VERSION" ]; then
     svn co http://svn.dojotoolkit.org/dojo/tags/$DOJO_VERSION
-    cd $DOJO_VERSION/buildscripts
-    patch -p0 < ../../0.4.3-buildUtil.js.patch
+#    cd $DOJO_VERSION/buildscripts
+#    patch -p0 < ../../0.4.3-buildUtil.js.patch
     ant fix-config
 fi
-cp cosmo-pim.js $DOJO_VERSION/src/
-cp cosmo-login.js $DOJO_VERSION/src/
-cd $DOJO_VERSION/buildscripts
+
+cp cosmo-pim.js $DOJO_VERSION/dojo
+cp cosmo-login.js $DOJO_VERSION/dojo/
+
+cd $DOJO_VERSION/util/buildscripts
 if [ "$1" == "release" ]; then
     ant -Ddocless=true -Dprofile=../../../cosmo clean release
 else
-    ant -Ddocless=true -Dprofile=core clean release
+    ./build.sh profile=standard action=clean,release
+#    ant -Ddocless=true -Dprofile=core clean release
 fi
-gzip -9c ../release/dojo/dojo.js > ../release/dojo/dojo.js.gzip-compressed
-gzip -9c ../release/dojo/src/cosmo-pim.js > ../release/dojo/src/cosmo-pim.js.gzip-compressed
-gzip -9c ../release/dojo/src/cosmo-login.js > ../release/dojo/src/cosmo-login.js.gzip-compressed
+
+cd ..
+
+gzip -9c ../release/dojo/dojo/dojo.js > ../release/dojo/dojo/dojo.js.gzip-compressed
+gzip -9c ../release/dojo/dojo/cosmo-pim.js > ../release/dojo/dojo/cosmo-pim.js.gzip-compressed
+gzip -9c ../release/dojo/dojo/cosmo-login.js > ../release/dojo/dojo/cosmo-login.js.gzip-compressed

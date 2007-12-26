b89cb9b99d1e3a38d19884a7af5702258d730fd4
br <br@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2007-12-26 22:07:31 +0000

    parseIso8601 was moved datetime.js as 'fromIso8601'
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/branches/dojo1_0@6455 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- cosmo/src/main/webapp/js/cosmo/datetime/util.js
+++ cosmo/src/main/webapp/js/cosmo/datetime/util.js
@@ -21,10 +21,6 @@ dojo.require("cosmo.datetime.Date");
 dojo.require("cosmo.util.string");
 cosmo.datetime.util = new function () {
 
-    //for iso8601 parsing
-    this._dateFormatString = "yyyyMMdd";
-    this._timeFormatString = "HHmmss";
-
     var stripZeroPat = /^0/;
 
     //note that these are now strings instead of int's
@@ -136,46 +132,6 @@ cosmo.datetime.util = new function () {
         return ret;
     };
 
-  this.parseISO8601 = function(str) {
-      //20071104T190000Z
-      var arr = str.split("T");
-      var datePart = arr[0];
-      var timePart = arr[1] || null;
-
-      if (timePart){
-          var utc = cosmo.util.string.endsWith(timePart, "Z");
-          if (utc){
-              timePart = timePart.substring(0, timePart.length - 1);
-          }
-          var date = dojo.date.locale.parse(datePart + " "+ timePart,
-              { datePattern: this._dateFormatString,
-              timePattern: this._timeFormatString});
-          
-          if (utc){
-              var year  = date.getFullYear(); 
-              var month = date.getMonth();
-              var day = date.getDate();
-              var hours = date.getHours();
-              var minutes = date.getMinutes();
-              var seconds = date.getSeconds();
-              var millis = date.getMilliseconds();
-
-              date = new Date();
-              date.setFullYear(year);
-              date.setUTCMonth(month);
-              date.setUTCDate(day);
-              date.setUTCHours(hours);
-              date.setUTCMinutes(minutes);
-              date.setUTCSeconds(seconds);
-              date.setUTCMilliseconds(millis);
-          }
-          return date;
-      } else {
-          return dojo.date.locale.parse(datePart,
-                                   { selector: 'date',
-                                     datePattern: this._dateFormatString});
-      }
-  }
 
 };
 

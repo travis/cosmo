1eb5f8b59d3864c2ca9b78d41e7aade8468758ab
travis <travis@5fd0b11a-f2f7-0310-948e-c8f80ebed640>
2008-01-25 20:06:55 +0000

    Rename CmpStore and add test.
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/branches/dojo1_0@6522 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- cosmo/src/main/webapp/js/cosmo/data/UserStore.js
+++ cosmo/src/main/webapp/js/cosmo/data/UserStore.js
@@ -1,6 +1,7 @@
 dojo.provide("cosmo.data.UserStore");
 dojo.require("cosmo.cmp");
 dojo.require("dojo.data.api.Write");
+dojo.require("dojo.DeferredList");
 
 dojo.declare("cosmo.data.UserStore", dojo.data.api.Write, {
     //	summary:
@@ -110,23 +111,35 @@ dojo.declare("cosmo.data.UserStore", dojo.data.api.Write, {
 		return this.isItem(something); //boolean
 	},
 
-	loadItem: function(/* object */ keywordArgs){
+	loadItem: function(/* object */ kwArgs){
 		//	summary:
 		//		Load a user
 		//	keywordArgs:
 		//		object containing the args for loadItem.  See dojo.data.api.Read.loadItem()
-        var scope keywordArgs.scope || dojo.global();
-        try{
-            keywordArgs.item._storeProp = this;
-            if (keywordArgs.onItem){
-                keywordArgs.onItem.call(scope, keywordArgs.item)
-                                        
+        var scope = keywordArgs.scope || dojo.global();
+        // If the item isn't loaded
+        if (!this.isItemLoaded(kwArgs.item)){
+            // And we have enough information to get the user
+            if (kwArgs.item && kwArgs.item.username){
+                // Get the user
+                var d = cosmo.cmp.getUser(kwArgs.item.username);
+                d.addCallback(function(user){
+                    kwArgs.item._storeProp = this;
+                    dojo.mixin(kwArgs.item, user)
+                    if (kwArgs.onItem)
+                        kwArgs.onItem.call(scope, kwArgs.item);
+                    return kwArgs.item;
+                });
+                d.addErrback(function(e){
+                    if (kwArgs.onError)
+                        kwArgs.onError.call(scope, e);
+                    return e;
+                });
+                return d;
+            } else {
+                kwArgs.onError.call(
+                    scope, new Error("Could not load user, don't have enough information"));
             }
-        } catch (e){
-            if (keywordArgs.onError){
-                keywordArgs.onError.call(scope, e);
-            }
-        }
 	},
 
     SORT_ASCENDING: "ascending",
@@ -242,7 +255,6 @@ dojo.declare("cosmo.data.UserStore", dojo.data.api.Write, {
 //	},
 	
 	save: function(/* object */ keywordArgs){
-        
 		//	summary:
         //      Save new/modified/deleted users
 		// 	description:
@@ -260,6 +272,11 @@ dojo.declare("cosmo.data.UserStore", dojo.data.api.Write, {
 		}
         deferreds.push(cosmo.cmp.deleteUsers(
             dojo.map(this._deletedItems, function(user){return user.username})));
+        var dl = new dojo.DeferredList(deferreds);
+        var scope = keywordArgs.scope || dojo.global();
+        dl.addCallback(dojo.hitch(scope, keywordArgs.onComplete));
+        dl.addErrback(dojo.hitch(scope, keywordArgs.onError));
+        return dl;
 	},
 
 	revert: function(){
@@ -267,9 +284,9 @@ dojo.declare("cosmo.data.UserStore", dojo.data.api.Write, {
 		//	Invalidate changes (new and/or modified elements)
 		// returns:
 		//	True
-		console.log("XmlStore.revert() _newItems=" + this._newItems.length);
-		console.log("XmlStore.revert() _deletedItems=" + this._deletedItems.length);
-		console.log("XmlStore.revert() _modifiedItems=" + this._modifiedItems.length);
+		console.log("CmpStore.revert() _newItems=" + this._newItems.length);
+		console.log("CmpStore.revert() _deletedItems=" + this._deletedItems.length);
+		console.log("CmpStore.revert() _modifiedItems=" + this._modifiedItems.length);
 		this._newItems = [];
 		this._restoreItems(this._deletedItems);
 		this._deletedItems = [];
@@ -277,8 +294,12 @@ dojo.declare("cosmo.data.UserStore", dojo.data.api.Write, {
 		this._modifiedItems = [];
 		return true; //boolean
 	},
+
+    _restoreItems: function(items){
+        // Since we're not doing any internal cacheing, this is a no-op
+    },
 	
-	isDirty: function(/* item? */ item){
+	isDirty: function(/* object? */ user){
 		//	summary:
 		//		Check whether an item is new, modified or deleted
 		//	description:
@@ -287,15 +308,15 @@ dojo.declare("cosmo.data.UserStore", dojo.data.api.Write, {
 		//		Otherwise, true is returned if there are any new, modified
 		//		or deleted items.
 		//	item:
-		//		An item (XML element) to check
+        //      A user hash to check.
 		//	returns:
 		//		True if an item or items are new, modified or deleted, otherwise
 		//		false
 		if (item) {
-			var element = this._getRootElement(item.element);
-			return (this._getItemIndex(this._newItems, element) >= 0 ||
-				this._getItemIndex(this._deletedItems, element) >= 0 ||
-				this._getItemIndex(this._modifiedItems, element) >= 0); //boolean
+            
+			return (this._getItemIndex(this._newItems, user) >= 0 ||
+				this._getItemIndex(this._deletedItems, user) >= 0 ||
+				this._getItemIndex(this._modifiedItems, user) >= 0); //boolean
 		}
 		else {
 			return (this._newItems.length > 0 ||
@@ -303,227 +324,14 @@ dojo.declare("cosmo.data.UserStore", dojo.data.api.Write, {
 				this._modifiedItems.length > 0); //boolean
 		}
 	},
-
-	_saveItem: function(item, keywordArgs, method){
-		if(method === "PUT"){
-			url = this._getPutUrl(item);
-		}else if(method === "DELETE"){
-			url = this._getDeleteUrl(item);
-		}else{ // POST
-			url = this._getPostUrl(item);
-		}
-		if(!url){
-			if(keywordArgs.onError){
-				keywordArgs.onError.call(scope, new Error("No URL for saving content: " + postContent));
-			}
-			return;
-		}
-
-		var saveArgs = {
-			url: url,
-			method: (method || "POST"),
-			contentType: "text/xml",
-			handleAs: "xml"
-		};
-		var saveHander;
-		if(method === "PUT"){
-			saveArgs.putData = this._getPutContent(item);
-			saveHandler = dojo.rawXhrPut(saveArgs);
-		}else if(method === "DELETE"){
-			saveHandler = dojo.xhrDelete(saveArgs);
-		}else{ // POST
-			saveArgs.postData = this._getPostContent(item);
-			saveHandler = dojo.rawXhrPost(saveArgs);
-		}
-		var scope = (keywordArgs.scope || dojo.global);
-		var self = this;
-		saveHandler.addCallback(function(data){
-			self._forgetItem(item);
-			if(keywordArgs.onComplete){
-				keywordArgs.onComplete.call(scope);
-			}
-		});
-		saveHandler.addErrback(function(error){
-			if(keywordArgs.onError){
-				keywordArgs.onError.call(scope, error);
-			}
-		});
-	},
-
-	_getPostUrl: function(item){
-		//	summary:
-		//		Generate a URL for post
-		//	description:
-		//		This default implementation just returns '_url'.
-		//		Sub-classes may override this method for the custom URL.
-		//	item:
-		//		An item to save
-		//	returns:
-		//		A post URL
-		return this._url; //string
-	},
-
-	_getPutUrl: function(item){
-		//	summary:
-		//		Generate a URL for put
-		//	description:
-		//		This default implementation just returns '_url'.
-		//		Sub-classes may override this method for the custom URL.
-		//	item:
-		//		An item to save
-		//	returns:
-		//		A put URL
-		return this._url; //string
-	},
-
-	_getDeleteUrl: function(item){
-		//	summary:
-		//		Generate a URL for delete
-		// 	description:
-		//		This default implementation returns '_url' with '_keyAttribute'
-		//		as a query string.
-		//		Sub-classes may override this method for the custom URL based on
-		//		changes (new, deleted, or modified).
-		// 	item:
-		//		An item to delete
-		// 	returns:
-		//		A delete URL
-		if (!this._url) {
-			return this._url; //string
-		}
-		var url = this._url;
-		if (item && this._keyAttribute) {
-			var value = this.getValue(item, this._keyAttribute);
-			if (value) {
-				url = url + '?' + this._keyAttribute + '=' + value;
-			}
-		}
-		return url;	//string
-	},
-
-	_getPostContent: function(item){
-		//	summary:
-		//		Generate a content to post
-		// 	description:
-		//		This default implementation generates an XML document for one
-		//		(the first only) new or modified element.
-		//		Sub-classes may override this method for the custom post content
-		//		generation.
-		//	item:
-		//		An item to save
-		//	returns:
-		//		A post content
-		var element = item.element;
-		var declaration = "<?xml version=\"1.0\"?>"; // FIXME: encoding?
-		return declaration + dojox.data.dom.innerXML(element); //XML string
-	},
-
-	_getPutContent: function(item){
-		//	summary:
-		//		Generate a content to put
-		// 	description:
-		//		This default implementation generates an XML document for one
-		//		(the first only) new or modified element.
-		//		Sub-classes may override this method for the custom put content
-		//		generation.
-		//	item:
-		//		An item to save
-		//	returns:
-		//		A post content
-		var element = item.element;
-		var declaration = "<?xml version=\"1.0\"?>"; // FIXME: encoding?
-		return declaration + dojox.data.dom.innerXML(element); //XML string
-	},
-
-/* internal API */
-
-	_getAttribute: function(tagName, attribute){
-		if(this._attributeMap){
-			var key = tagName + "." + attribute;
-			var value = this._attributeMap[key];
-			if(value){
-				attribute = value;
-			}else{ // look for global attribute
-				value = this._attributeMap[attribute];
-				if(value){
-					attribute = value;
-				}
-			}
-		}
-		return attribute; //object
-	},
-
-	_getItem: function(element){
-		return new dojox.data.XmlItem(element, this); //object
-	},
-
-	_getItemIndex: function(items, element){
-		for(var i = 0; i < items.length; i++){
-			if(items[i].element === element){
-				return i; //int
-			}
-		}
-		return -1; //int
-	},
-
-	_backupItem: function(item){
-		var element = this._getRootElement(item.element);
-		if(	this._getItemIndex(this._newItems, element) >= 0 ||
-			this._getItemIndex(this._modifiedItems, element) >= 0){
-			return; // new or already modified
-		}
-		if(element != item.element){
-			item = this._getItem(element);
-		}
-		item._backup = element.cloneNode(true);
-		this._modifiedItems.push(item);
-	},
-
-	_restoreItems: function(items){
-
-		dojo.forEach(items,function(item){ 
-			if(item._backup){
-				item.element = item._backup;
-				item._backup = null;
-			}
-		},this); 
-	},
-
-	_forgetItem: function(item){
-		var element = item.element;
-		var index = this._getItemIndex(this._newItems, element);
-		if(index >= 0){
-			this._newItems.splice(index, 1);
-		}
-		index = this._getItemIndex(this._deletedItems, element);
-		if(index >= 0){
-			this._deletedItems.splice(index, 1);
-		}
-		index = this._getItemIndex(this._modifiedItems, element);
-		if(index >= 0){
-			this._modifiedItems.splice(index, 1);
-		}
-	},
-
-	_getDocument: function(element){
-		if(element){
-			return element.ownerDocument;  //DOMDocument
-		}else if(!this._document){
-			return dojox.data.dom.createDocument(); // DOMDocument
-		}
-	},
-
-	_getRootElement: function(element){
-		while(element.parentNode){
-			element = element.parentNode;
-		}
-		return element; //DOMElement
-	}
-
+    
+    _getItemIndex: function(items, user){
+        for (var i = 0; i < items.length; i++){
+            if (items[i].username == user.username){
+                return i;
+            }
+        }
+        return -1;
+    }
 });
 
-
-function getMyCollections(){
-    var r = cosmo.util.auth.getAuthorizedRequest();
-
-}

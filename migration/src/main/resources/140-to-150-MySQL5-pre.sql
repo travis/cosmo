1b3dab1b74fbb371c29fe7763e479a232710b0e5
Travis Vachon <travis@osafoundation.org>
2008-04-14 17:06:49 +0000

    Migration script updates.
    Still needs testing, but other migration updates for trunk haven't happened yet.
    
    
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@6832 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- migration/src/main/resources/140-to-150-MySQL5-pre.sql
+++ migration/src/main/resources/140-to-150-MySQL5-pre.sql
@@ -2,6 +2,14 @@
 
 # migrate data
 alter table collection_item add column createdate bigint
+alter table tickets add column createdate bigint
+alter table tickets add column modifydate bigint
+alter table tickets add column etag varchar(255)
 
 # initialize createdate
 update collection_item set createdate=1
+
+# initialize new ticket fields
+update tickets set createdate=1
+update tickets set modifydate=1
+update tickets set etag=''

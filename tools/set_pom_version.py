792c1689a311f4bf3b8300b1e2f4dfe444623c64
Travis Vachon <travis@osafoundation.org>
2008-05-06 20:34:52 +0000

    Update pom version script.
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/branches/0.15@6945 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- tools/set_pom_version.py
+++ tools/set_pom_version.py
@@ -19,16 +19,13 @@ import getopt
 import re
 
 parent_pom = "pom.xml" 
-dojo_pom = "dojo/pom.xml"
 set_cosmo_poms = [parent_pom,
                   "jsonrpc-java-js/pom.xml",
-                   "olson/pom.xml",
-                   "migration/pom.xml",
-                   "snarf/pom.xml",
-                   "dojo/pom.xml",
-                   "cosmo/pom.xml"]
-set_dojo_poms = [dojo_pom,
-                 "cosmo/pom.xml"]
+                  "olson/pom.xml",
+                  "migration/pom.xml",
+                  "snarf/pom.xml",
+                  "cosmo-js/pom.xml",
+                  "cosmo/pom.xml"]
 base_dir = "./"
 
 def add_base(path):
@@ -36,10 +33,10 @@ def add_base(path):
 
 def usage():
     print """Usage:
-    Print current version numbers:
+    Print current version number:
         %s -n 
     Change version numbers:
-        %s -v cosmo_version -d dojo_version [-b base_directory]
+        %s -v cosmo_version [-b base_directory]
 """ % (sys.argv[0], sys.argv[0])
 
 def get_new_versions():
@@ -54,22 +51,19 @@ def get_new_versions():
         
     for o,a in opts:
         if o == "-n":
-            print "Cosmo version:%s\n Dojo version:%s" % get_old_versions()
+            print get_old_versions()
             sys.exit(0)
         if o == "-v":
             version = a
-        if o == "-d":
-            dojo_version = a
         if o == "-b":
             global base_dir
             base_dir = a
-    return version, dojo_version
+    return version
 
 def get_old_versions():
     try:
         old_version = re.search("<project>.*<groupId>org\.osaf\.cosmo</groupId>.*<version>(.*)</version>.*<name>.*</project>", open(add_base(parent_pom)).read(), re.DOTALL).group(1)    
-        old_dojo_version = re.search("<project>.*<artifactId>dojo</artifactId>.*<version>(.*)</version>.*<name>.*</project>", open(add_base(dojo_pom)).read(), re.DOTALL).group(1)    
-        return old_version, old_dojo_version
+        return old_version
 
     except AttributeError, e:
         print "Could not figure out old version number."
@@ -84,32 +78,19 @@ def replace_string(filename, find, replace):
     new_file.close()
 
 if __name__ == "__main__":
-    
-    required_files = [parent_pom] + [dojo_pom] + set_cosmo_poms + set_dojo_poms
-
+    required_files = set_cosmo_poms
     for file_path in required_files:
         if not os.path.exists(file_path):
             print file_path + " does not exist."
             sys.exit(1)
             
-    version, dojo_version = get_new_versions()
-    old_version, old_dojo_version = get_old_versions()
+    version = get_new_versions()
+    old_version = get_old_versions()
 
-    if version == None or dojo_version == None:
-        print "Error: need both version numbers."
+    if version == None:
+        print "Error: need version number."
         usage()
         sys.exit(1)
 
     for filename in set_cosmo_poms:
         replace_string(filename, old_version, version)
-
-    for filename in set_dojo_poms:
-        replace_string(filename, old_dojo_version, dojo_version)
-        
-        
-    
-    
-
-    
-    
-

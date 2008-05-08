07912a3ee0eb9dc1f8fdc56aa91d4900aa05365a
Travis Vachon <travis@osafoundation.org>
2008-05-08 01:15:39 +0000

    Add port of Paul Johnston's sha1 JS.
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@6960 5fd0b11a-f2f7-0310-948e-c8f80ebed640

---
+++ cosmo-js/src/cosmo/util/tests/sha1.js
@@ -0,0 +1,21 @@
+dojo.provide("cosmo.util.tests.sha1");
+dojo.require("cosmo.util.sha1");
+
+(function(){
+	var message="\u00E5The rain in Spain falls mainly on the plain.";
+	var base64='FODN+eoU0iYFAC1ce4u46Ymg0BY=';
+	var hex='14e0cdf9ea14d22605002d5c7b8bb8e989a0d016';
+	var s='\x14\xe0\xcd\xf9\xea\x14\xd2&\x05\x00-\\{\x8b\xb8\xe9\x89\xa0\xd0\x16';
+	var cus = cosmo.util.sha1;
+	tests.register("dojox.encoding.tests.digests.SHA1", [
+		function testBase64Compute(t){
+			t.assertEqual(base64, cus.b64(message));
+		},
+		function testHexCompute(t){
+			t.assertEqual(hex, cus.hex(message));
+		},
+		function testStringCompute(t){
+			t.assertEqual(s, cus.str(message));
+		}
+	]);
+})();

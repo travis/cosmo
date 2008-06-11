22c4283fe908a68c91de55cc1fdb0ac63df02264
Jeffrey Harris <jeffrey@osafoundation.org>
2008-06-11 21:45:18 +0000

    - Temporary working stuff for settings, should be amended
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@7077 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- cosmo-js/src/cosmo/account/settings.js
+++ cosmo-js/src/cosmo/account/settings.js
@@ -90,6 +90,7 @@ cosmo.account.settings = new function () {
      * Editing account info flushes the cache so it gets updated
      * from the server when the box is invoked again
      */
+//     this.showDialog =
     this.showDialog = function () {
         var o = {};
         var s = document.createElement('span'); // Throwaway node to avoid doc reflow
@@ -287,3 +288,139 @@ cosmo.account.settings = new function () {
 
 
 };
+
+dojo.require("dijit.Dialog");
+
+dojo.declare("cosmo.ui.widget.SettingsDialog", [dijit._Widget, dijit._Templated],
+{
+    widgetsInTemplate: true,
+    templatePath: dojo.moduleUrl("cosmo", 'ui/widget/templates/SettingsDialog.html'),
+
+    // init params
+    store: null,
+    collection: null,
+    ticketStore: null,
+    hideInvite: true,
+    hideDestroy: true,
+
+    // collection or subscription object
+    displayName: "",
+    urls: null,
+
+    // attach points
+    displayName: null,
+    instructionsContainer: null,
+    instructionsSelector: null,
+    ticketContainer: null,
+    inviteSection: null,
+    readOnlyInviteLink: null,
+    readWriteInviteLink: null,
+    inviteButton: null,
+    destroyButton: null,
+
+    readOnlyTicket: null,
+    readWriteTicket: null,
+
+    constructor: function(){
+        this.l10n = dojo.i18n.getLocalization("cosmo.ui.widget", "SharingDialog");
+    },
+
+    // Functions for subscription instructions
+    instructionsOnClick: function(e, instructionsKey, urls){
+        e.preventDefault();
+        var instructions = dojo.string.substitute(this.l10n[instructionsKey + "Instructions"], urls || this.urls);
+        var dialog = new dijit.Dialog({title: this.l10n[instructionsKey + "InstructionsTitle"]});
+        dialog.setContent(instructions);
+        dialog.startup();
+        dialog.show();
+    },
+
+    atomOnClick: function(e){
+        if (this.ticketStore){
+            var d = this.getReadOnlyTicket();
+            d.addCallback(dojo.hitch(this,
+            function(ticket){
+                this.instructionsOnClick(e, "feedReader",
+                {atom: new dojo._Url(this.getTicketedUrl(this.urls.atom.uri, ticket))});
+            }));
+            return d;
+        } else this.instructionsOnClick(e, "feedReader");
+
+    },
+
+    chandlerOnClick: function(e){
+        this.instructionsOnClick(e, "chandler");
+    },
+
+    davOnClick: function(e){
+        this.instructionsOnClick(e, "dav");
+    },
+
+    iCalOnClick: function(e){
+        this.instructionsOnClick(e, "iCal", dojo.mixin({
+            webcalProtocol: new dojo._Url(this.urls.webcal.uri.replace("https", "webcal").replace("http", "webcal"))
+        }, this.urls));
+    },
+
+    onTicket: function(ticket){
+        if (!this.readOnlyTicket && (this.ticketStore.getValue(ticket, "type") == "read-only"))
+            this.readOnlyTicket = ticket;
+        else if (!this.readWriteTicket && (this.ticketStore.getValue(ticket, "type") == "read-write"))
+            this.readWriteTicket = ticket;
+        return ticket;
+    },
+
+    getReadOnlyTicket: function(){
+        if (this.readOnlyTicket){
+            var d = new dojo.Deferred();
+            d.callback(this.readOnlyTicket);
+            return d;
+        } else {
+            return this.createTicket("read-only").addCallback(dojo.hitch(this, this.onTicket));
+        }
+    },
+
+    getReadWriteTicket: function(){
+        if (this.readWriteTicket){
+            var d = new dojo.Deferred();
+            d.callback(this.readWriteTicket);
+            return d;
+        } else {
+            return this.createTicket("read-write").addCallback(dojo.hitch(this, this.onTicket));
+        }
+    },
+
+    getTwoTickets: function(){
+        var rod = this.getReadOnlyTicket();
+        var rwd = this.getReadWriteTicket();
+        return new dojo.DeferredList([rod, rwd]);
+    },
+
+    invite: function(){
+        console.log("foo");
+        var d = this.getTwoTickets();
+        d.addCallback(dojo.hitch(this,
+            function(){
+                var tickets = {
+                    "read-only": this.readOnlyTicket,
+                    "read-write": this.readWriteTicket
+                };
+                this.updateInviteLinks(tickets);
+                this.showInviteLinks();
+            }));
+    },
+
+    updateInviteLinks: function(tickets){
+        var ro = tickets["read-only"];
+        var rw = tickets["read-write"];
+        var baseUrl = this.urls.html.uri;
+        if (ro) this.readOnlyInviteLink.setAttribute("href", this.getTicketedUrl(baseUrl, ro));
+        if (rw) this.readWriteInviteLink.setAttribute("href", this.getTicketedUrl(baseUrl, rw));
+    },
+
+    // lifecycle methods
+    postMixInProperties: function(){
+    },
+    postCreate: function(){
+    }
+});

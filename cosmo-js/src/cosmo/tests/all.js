646bde6b19da1ed9c5ae9967351a6fade458ef85
Travis Vachon <travis@osafoundation.org>
2008-06-17 18:29:30 +0000

    Clean up unit tests, giving appropriate titles to html files and tweaking full test run file names.
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@7108 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- cosmo-js/src/cosmo/tests/all.js
+++ cosmo-js/src/cosmo/tests/all.js
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-dojo.provide("cosmo.tests.full");
+dojo.provide("cosmo.tests.all");
 try{
 	dojo.require("cosmo.tests.module");
 	dojo.require("cosmo.util.tests.module");

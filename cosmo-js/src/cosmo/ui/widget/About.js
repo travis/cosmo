0e468117ab60796ca60b6393fa797713952d325e
Jeffrey Harris <jeffrey@osafoundation.org>
2008-06-24 00:59:19 +0000

    Assorted UI tweaks for bug 12059.
    git-svn-id: svn+ssh://svn.osafoundation.org/svn/server/cosmo/trunk@7173 5fd0b11a-f2f7-0310-948e-c8f80ebed640

--- cosmo-js/src/cosmo/ui/widget/About.js
+++ cosmo-js/src/cosmo/ui/widget/About.js
@@ -27,31 +27,30 @@ dojo.declare("cosmo.ui.widget.About", [dijit._Widget, dijit._Templated], {
             node.style.margin = 'auto';
             node.style.width = '100%';
             node.style.height = "230px";
-            node.style.overflowY = 'scroll';
+            node.style.overflowY = 'auto';
 
             // Image
             d = _createElem('div');
-            d.style.paddingTop = '16px';
             var img = _createElem('img');
-            img.src = cosmo.env.getImageUrl( _("App.LogoUri"));
+            img.src = cosmo.env.getImageUrl( _("About.LogoUri"));
             d.appendChild(img);
             node.appendChild(d);
             // Version
             d = _createElem('div');
-            d.style.marginTop = '-4px';
+            d.style.marginTop = '4px';
             d.innerHTML = _('About.Version', cosmo.env.getVersion());
             node.appendChild(d);
             // License text
             d = _createElem('div');
-            d.style.marginTop = '24px';
+            d.style.marginTop = '12px';
             d.innerHTML = this.strings.license;
             node.appendChild(d);
             // Info text
             d = _createElem('div');
-            d.style.marginTop = '12px';
+            d.style.marginTop = '2px';
             d.innerHTML = this.strings.info;
             node.appendChild(d);
-            
+
             d = _createElem('div');
             d.className = "notices";
             var noticesDeferred = dojo.xhrGet({url: cosmo.env.getFullUrl("Notices")});
